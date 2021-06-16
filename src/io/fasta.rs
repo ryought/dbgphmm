5289e3a3bf8c6746a007bb8c219e97e83fab2044
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2021-06-16 23:13:15 +0900

    read base sanitizer

--- src/io/fasta.rs
+++ src/io/fasta.rs
@@ -1,15 +1,36 @@
 use crate::dbg::{DbgHash, DBG};
 use crate::kmer::kmer::Kmer;
 use bio::io::fasta;
+use log::warn;
+
+pub fn sanitize_bases(seq: &[u8]) -> Vec<u8> {
+    seq.iter()
+        .enumerate()
+        .map(|(i, base)| match base {
+            b'A' | b'a' => b'A',
+            b'C' | b'c' => b'C',
+            b'G' | b'g' => b'G',
+            b'T' | b't' => b'T',
+            b'N' | b'n' => {
+                warn!("ambiguous detected `n` in bases[{}]", i);
+                b'N'
+            }
+            &c => {
+                warn!("informal base `{}` detected in bases[{}]", c as char, i);
+                b'N'
+            }
+        })
+        .collect()
+}
 
 pub fn parse_kmers_and_copy_nums(filename: &str, k: usize) -> (Vec<Kmer>, Vec<u32>) {
     // and returns (kmers, copy_nums) that can be used as an input
     // of DbgPHMM::new.
-    let mut reader = fasta::Reader::from_file(filename).unwrap();
+    let reader = fasta::Reader::from_file(filename).unwrap();
     let mut d = DbgHash::new();
     for result in reader.records() {
         let record = result.unwrap();
-        for window in record.seq().windows(k) {
+        for window in sanitize_bases(record.seq()).windows(k) {
             let kmer = Kmer::from(window);
             d.add(kmer, 1);
         }
@@ -24,7 +45,7 @@ pub fn parse_reads(filename: &str) -> Vec<Vec<u8>> {
     let mut reads: Vec<Vec<u8>> = Vec::new();
     for result in reader.records() {
         let record = result.unwrap();
-        reads.push(record.seq().to_vec());
+        reads.push(sanitize_bases(record.seq()));
     }
     reads
 }
