92996c9071cc5e35cfb93b8497061c434936586e
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2021-07-01 15:37:41 +0900

    cdbg completed

--- src/compressed_dbg.rs
+++ src/compressed_dbg.rs
@@ -1,6 +1,8 @@
 use crate::cycles;
 use crate::dbg::{DbgHash, DBG};
+use crate::graph::Node;
 use crate::kmer::kmer::{null_kmer, Kmer};
+use crate::prob::Prob;
 use fnv::FnvHashMap as HashMap;
 
 /// indexed and compressed dbg
@@ -10,12 +12,12 @@ use fnv::FnvHashMap as HashMap;
 pub struct CompressedDBG {
     k: usize,
     n_kmers: usize,
-    childs: Vec<Vec<usize>>,
-    parents: Vec<Vec<usize>>,
+    childs: Vec<Vec<Node>>,
+    parents: Vec<Vec<Node>>,
     kmers: Vec<Kmer>,
-    ids: HashMap<Kmer, usize>,
+    ids: HashMap<Kmer, Node>,
     emissions: Vec<u8>,
-    cycles: Vec<Vec<usize>>,
+    cycles: Vec<Vec<Node>>,
 }
 
 impl CompressedDBG {
@@ -26,13 +28,13 @@ impl CompressedDBG {
         // assign an index to each kmers
         let kmers: Vec<Kmer> = dbg.kmers();
         let n_kmers = kmers.len();
-        let mut ids: HashMap<Kmer, usize> = HashMap::default();
+        let mut ids: HashMap<Kmer, Node> = HashMap::default();
         for (i, kmer) in kmers.iter().enumerate() {
-            ids.insert(kmer.clone(), i);
+            ids.insert(kmer.clone(), Node(i));
         }
 
-        let mut childs: Vec<Vec<usize>> = Vec::new();
-        let mut parents: Vec<Vec<usize>> = Vec::new();
+        let mut childs: Vec<Vec<Node>> = Vec::new();
+        let mut parents: Vec<Vec<Node>> = Vec::new();
         let mut emissions: Vec<u8> = Vec::new();
         for kmer in kmers.iter() {
             childs.push(
@@ -52,7 +54,7 @@ impl CompressedDBG {
 
         let root = null_kmer(k - 1);
         let s = cycles::DbgTree::new(dbg, &root);
-        let cycles: Vec<Vec<usize>> = s
+        let cycles: Vec<Vec<Node>> = s
             .cycle_keys()
             .iter()
             .map(|key| {
@@ -78,20 +80,64 @@ impl CompressedDBG {
     pub fn n_kmers(&self) -> usize {
         self.n_kmers
     }
-    pub fn childs(&self, v: usize) -> &[usize] {
-        &self.childs[v]
+    pub fn iter_nodes(&self) -> impl std::iter::Iterator<Item = Node> {
+        (0..self.n_kmers).map(|i| Node(i))
     }
-    pub fn parents(&self, v: usize) -> &[usize] {
-        &self.parents[v]
+    pub fn childs(&self, v: Node) -> &[Node] {
+        &self.childs[v.0]
     }
-    pub fn is_adjacent(&self, v: usize, w: usize) -> bool {
+    pub fn parents(&self, v: Node) -> &[Node] {
+        &self.parents[v.0]
+    }
+    pub fn is_adjacent(&self, v: Node, w: Node) -> bool {
         self.childs(v).iter().any(|&u| u == w)
     }
-    pub fn emission(&self, v: usize) -> u8 {
-        self.emissions[v]
+    pub fn emission(&self, v: Node) -> u8 {
+        self.emissions[v.0]
     }
-    pub fn kmer(&self, v: usize) -> &Kmer {
-        &self.kmers[v]
+    pub fn is_emitable(&self, v: Node) -> bool {
+        self.emission(v) != b'N'
+    }
+    pub fn kmer(&self, v: Node) -> &Kmer {
+        &self.kmers[v.0]
+    }
+    pub fn n_cycles(&self) -> usize {
+        self.cycles.len()
+    }
+    pub fn cycle_components(&self, i: usize) -> &[Node] {
+        &self.cycles[i]
+    }
+    /// Calc transition probabilities from copy numbers of each kmers
+    /// given
+    /// childs[i] = [0, 1, 2, 3]
+    /// this computes
+    /// trans_probs[i] = [p(i->0), p(i->1), p(i->2), p(i->3)]
+    pub fn copy_num_to_trans_prob(&self, copy_nums: &[u32]) -> Vec<Vec<Prob>> {
+        // TODO assert copy_nums has the same shape
+        //
+        (0..self.n_kmers())
+            .map(|i| {
+                let v = Node(i);
+                let cns: Vec<u32> = self.childs(v).iter().map(|&w| copy_nums[w.0]).collect();
+                let total_cn: u32 = cns.iter().sum();
+                cns.iter()
+                    .map(|&cn| Prob::from_prob(f64::from(cn) / f64::from(total_cn)))
+                    .collect()
+            })
+            .collect()
+    }
+    /// check copy_nums consistency by
+    /// (total cn of parents) == (total cn of childs of one of the parents)
+    /// TODO
+    pub fn is_consistent_copy_num(&self, copy_nums: &[u32]) -> bool {
+        true
+    }
+    pub fn total_emitable_copy_num(&self, copy_nums: &[u32]) -> u32 {
+        (0..self.n_kmers())
+            .map(|i| Node(i))
+            .filter(|&v| self.is_emitable(v))
+            .map(|v| copy_nums[v.0])
+            .sum()
     }
 }
 
@@ -102,6 +148,19 @@ pub fn test() {
     // d.add_seq(b"ATCTCGATCTTGATAGATCG", 8);
     // println!("{}", d.as_dot());
     let cdbg = CompressedDBG::from(&d, 8);
-    println!("#cycles: {}", cdbg.cycles.len());
-    println!("cycle: {:?}", cdbg.cycles[0]);
+
+    for (i, v) in cdbg.iter_nodes().enumerate() {
+        println!(
+            "node #{}: {:?} childs:{:?} parents:{:?}",
+            i,
+            v,
+            cdbg.childs(v),
+            cdbg.parents(v)
+        );
+    }
+
+    println!("#cycles: {}", cdbg.n_cycles());
+    for i in 0..cdbg.n_cycles() {
+        println!("cycle#{}: {:?}", i, cdbg.cycle_components(i));
+    }
 }
