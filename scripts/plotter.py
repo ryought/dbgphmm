a36c42cd6f3431ba92cf40f842d094555facb3ef
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2021-07-08 18:38:02 +0900

    plotter updated (iteration vs score/temp/...etc)

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -10,6 +10,8 @@ import json
 import csv
 from dataclasses import dataclass
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 @dataclass
@@ -29,13 +31,8 @@ class OptimizeLog:
     next_size: int
     next_state: list
 
-def main():
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('tsv_filename', type=str, help='tsv created by dbgphmm optimize')
-    parser.add_argument('json_filename', type=str, help='json created by dbgphmm stats')
-    args = parser.parse_args()
-
-    with open(args.tsv_filename) as f:
+def parse_logs(tsv_filename):
+    with open(tsv_filename) as f:
         logs = []
         reader = csv.reader(f, delimiter='\t')
         for row in reader:
@@ -56,9 +53,57 @@ def main():
                 next_state=json.loads(row[13]),
             )
             logs.append(log)
+    return logs
 
-    with open(args.json_filename) as f:
+def parse_stats(json_filename):
+    with open(json_filename) as f:
         stats = json.load(f)
+    return stats
+
+def dist(state_a, state_b):
+    return np.linalg.norm(np.array(state_a) - np.array(state_b))
+
+def plot_temp(logs, stats, filename):
+    x = np.array([log.id for log in logs])
+
+    plt.subplot(4, 1, 1)
+    plt.plot(x, np.array([log.temp for log in logs]))
+    plt.ylabel('temp')
+
+    plt.subplot(4, 1, 2)
+    plt.plot(x, np.array([log.now_score for log in logs]))
+    plt.plot(x, np.array([log.now_score_prior for log in logs]))
+    plt.plot(x, np.array([log.now_score_forward for log in logs]))
+    plt.ylabel('score')
+
+    plt.subplot(4, 1, 3)
+    plt.plot(x, np.array([log.now_size for log in logs]))
+    plt.ylabel('size')
+
+    plt.subplot(4, 1, 4)
+    plt.plot(x, np.array([sum(log.now_state) for log in logs]))
+    plt.plot(x, np.array([dist(log.now_state, logs[0].now_state) for log in logs]))
+    plt.ylabel('dist')
+
+    plt.xlabel('id')
+    plt.savefig(filename)
+
+def plot_basis(logs, stats, filename):
+    plt.savefig(filename)
+
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('tsv_filename', type=str, help='tsv created by dbgphmm optimize')
+    parser.add_argument('json_filename', type=str, help='json created by dbgphmm stats')
+    parser.add_argument('--start-from-true-copy-nums', action='store_true', help='Bool flag( --debug or no )')
+    args = parser.parse_args()
+
+    logs = parse_logs(args.tsv_filename)
+    stats = parse_stats(args.json_filename)
+
+    if args.start_from_true_copy_nums:
+        plot_temp(logs, stats, args.tsv_filename + '.tempscore.png')
+        plot_basis(logs, stats, args.tsv_filename + '.basis.png')
 
 if __name__ == '__main__':
     main()
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,27 @@
 
-./target/release/dbgphmm -k 8 optimize data/r1.fa --true-dbg-fa data/d1.fa -M 10 -V 10 -I 100 -R 0.8 --parallel --start-from-true-copy-nums > data/r1.tsv
-./target/release/dbgphmm -k 8 stat data/r1.fa > data/r1.json
+# ./target/release/dbgphmm -k 8 optimize data/r1.fa --true-dbg-fa data/d1.fa -M 10 -V 10 -I 100 -R 0.8 --parallel --start-from-true-copy-nums > data/r1.tsv
+# ./target/release/dbgphmm -k 8 stat data/r1.fa > data/r1.json
+
+
+function from_zero () {
+  # initial temperature
+  T=100
+  # number of iteration
+  I=500
+  # cooling rate
+  R=0.95
+  ./target/release/dbgphmm -k 8 optimize data/r1.fa --true-dbg-fa data/d1.fa -M 10 -V 10 -T $T -I $I -R $R --parallel > data/r1_f0_T${T}_R${R}_I${I}.tsv
+}
+
+function from_true () {
+  # initial temperature
+  T=1000
+  # number of iteration
+  I=500
+  # cooling rate
+  R=1.0
+  # ./target/release/dbgphmm -k 8 optimize data/r1.fa --true-dbg-fa data/d1.fa -M 10 -V 10 -T $T -I $I -R $R --parallel --start-from-true-copy-nums > data/r1_ft_T${T}_R${R}_I${I}.tsv
+  python scripts/plotter.py data/r1_ft_T${T}_R${R}_I${I}.tsv data/r1.json
+}
+
+from_true
