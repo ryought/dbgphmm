fbce5f4e6d21a81b1ba9e555ef96930e4ca540a2
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2021-07-11 11:30:46 +0900

    move cli opts definitions main.rs -> cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -3,11 +3,217 @@ use crate::hmm::base::PHMM;
 use crate::hmm::params::PHMMParams;
 use crate::hmm::sampler::PHMMSampler;
 use crate::kmer::kmer::Kmer;
+use crate::prob::Prob;
 use crate::*;
+use clap::{AppSettings, Clap};
 use log::{info, warn};
 use rand::prelude::*;
 use rand_xoshiro::Xoshiro256PlusPlus;
 use rayon::prelude::*;
+
+/// de bruijn graph + profile HMM optimization package
+#[derive(Clap)]
+#[clap(version = "0.1", author = "ryought <ryonakabayashi@gmail.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+    /// kmer size
+    #[clap(short = 'k', long, default_value = "8")]
+    kmer_size: usize,
+    /// mismatch probability
+    #[clap(long, default_value = "0.01")]
+    p_mismatch: f64,
+    /// gap open probability
+    #[clap(long, default_value = "0.01")]
+    p_gap_open: f64,
+    /// gap extension probability
+    #[clap(long, default_value = "0.01")]
+    p_gap_ext: f64,
+    /// end probability
+    #[clap(long, default_value = "0.00001")]
+    p_end: f64,
+    /// number of consecutive dels to consider
+    #[clap(long, default_value = "3")]
+    n_max_gaps: u32,
+    /// Print debug info
+    #[clap(short, long)]
+    debug: bool,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Generate(Generate),
+    Stat(Stat),
+    Compare(Compare),
+    Sample(Sample),
+    Forward(Forward),
+    Optimize(Optimize),
+    Sandbox(Sandbox),
+}
+
+/// Generate a random sequence
+#[derive(Clap)]
+struct Generate {
+    /// sequence length to generate
+    #[clap(short = 'l', long)]
+    length: usize,
+    /// random seed
+    #[clap(short = 's', long, default_value = "0")]
+    seed: u64,
+}
+
+/// Show de bruijn graph statistics
+#[derive(Clap)]
+struct Stat {
+    /// dbg fasta file
+    dbg_fa: String,
+}
+
+/// Compare two dbg, e.g. between two refs or ref/reads
+#[derive(Clap)]
+struct Compare {
+    /// self dbg fasta (e.g. reference fasta)
+    self_dbg_fa: String,
+    /// other dbg fasta (e.g. reads fasta)
+    other_dbg_fa: String,
+}
+
+/// Sample reads from the model
+#[derive(Clap)]
+struct Sample {
+    /// dbg fasta file
+    dbg_fa: String,
+    /// read length to generate
+    #[clap(short = 'l', long)]
+    length: u32,
+    /// number of reads
+    #[clap(short = 'n', long)]
+    n_reads: u32,
+    /// random seed
+    #[clap(short = 's', long, default_value = "0")]
+    seed: u64,
+    /// Require all reads to start from the head node
+    #[clap(long)]
+    start_from_head: bool,
+}
+
+/// Calculate probability that the model produces the reads
+#[derive(Clap)]
+struct Forward {
+    /// dbg fasta file
+    dbg_fa: String,
+    /// reads fasta file
+    reads_fa: String,
+    /// Use rayon to parallel calculation
+    #[clap(long)]
+    parallel: bool,
+}
+
+/// Optimize the model to fit the reads
+#[derive(Clap)]
+struct Optimize {
+    /// reads fasta file
+    reads_fa: String,
+    /// true dbg fasta file for validation
+    #[clap(long)]
+    true_dbg_fa: Option<String>,
+    /// initial temperature in simulated annealing
+    #[clap(short = 'T', long, default_value = "1.0")]
+    init_temp: f64,
+    /// cooling rate in simulated annealing
+    #[clap(short = 'R', long, default_value = "0.8")]
+    cooling_rate: f64,
+    /// cooling rate in simulated annealing
+    #[clap(short = 'I', long, default_value = "100")]
+    n_iteration: u64,
+    /// average of genome size in prior distribution
+    #[clap(short = 'M', long)]
+    genome_size_ave: u32,
+    /// std. dev. of genome size in prior distribution
+    #[clap(short = 'V', long)]
+    genome_size_std_var: u32,
+    /// Only uses prior score as a state score (not forward score)
+    #[clap(long)]
+    prior_only: bool,
+    /// Start from true copy numbers infered from true_dbg_fa
+    #[clap(long)]
+    start_from_true_copy_nums: bool,
+    /// Dump seqs as FASTA after optimizing
+    #[clap(long)]
+    dump_seqs: bool,
+    /// Use rayon to parallel calculation
+    #[clap(long)]
+    parallel: bool,
+    /// random seed
+    #[clap(short = 's', long, default_value = "11")]
+    seed: u64,
+}
+
+/// Sandbox for debugging
+#[derive(Clap)]
+struct Sandbox {}
+
+pub fn main() {
+    // parse options
+    let opts: Opts = Opts::parse();
+    let param = hmm::params::PHMMParams::new(
+        prob::Prob::from_prob(opts.p_mismatch),
+        prob::Prob::from_prob(opts.p_gap_open),
+        prob::Prob::from_prob(opts.p_gap_ext),
+        prob::Prob::from_prob(opts.p_end),
+        opts.n_max_gaps,
+    );
+    let k = opts.kmer_size;
+
+    match opts.subcmd {
+        SubCommand::Generate(t) => {
+            cli::generate(t.length, t.seed);
+        }
+        SubCommand::Stat(t) => {
+            cli::stat(t.dbg_fa, k);
+        }
+        SubCommand::Compare(t) => {
+            cli::compare(t.self_dbg_fa, t.other_dbg_fa, k);
+        }
+        SubCommand::Sample(t) => {
+            cli::sample(
+                t.dbg_fa,
+                t.length,
+                t.n_reads,
+                k,
+                t.seed,
+                param,
+                t.start_from_head,
+            );
+        }
+        SubCommand::Forward(t) => {
+            cli::forward(t.dbg_fa, t.reads_fa, k, param, t.parallel);
+        }
+        SubCommand::Optimize(t) => match t.true_dbg_fa {
+            Some(dbg_fa) => cli::optimize_with_answer(
+                dbg_fa,
+                t.reads_fa,
+                k,
+                param,
+                t.init_temp,
+                t.cooling_rate,
+                t.n_iteration,
+                t.genome_size_ave,
+                t.genome_size_std_var,
+                t.prior_only,
+                t.start_from_true_copy_nums,
+                t.dump_seqs,
+                t.parallel,
+                t.seed,
+            ),
+            None => cli::optimize(t.reads_fa, k, param),
+        },
+        SubCommand::Sandbox(t) => {
+            cli::sandbox();
+        }
+    }
+}
 
 pub fn generate(length: usize, seed: u64) {
     let v = random_seq::generate(length, seed);
