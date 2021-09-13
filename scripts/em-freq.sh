331e9494c1b22fb820731e0686f0b7c142046547
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2021-09-13 12:49:13 +0900

    added script to test

---
+++ scripts/em-freq.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd data/em-freq
+
+dbgphmm -k 8 generate -l 50 > g1.fa
+dbgphmm -k 8 stat g1.fa > g1.json
+dbgphmm -k 30 sample g1.fa --length 1000 --n-reads 10 --start-from-head > g1_r1.fa
+dbgphmm -k 8 benchmark g1_r1.fa g1.fa -V 10 --init-state read-count full-em --depth-scheduler linear-gradient > g1_k8.tsv
+dbgphmm -k 16 benchmark g1_r1.fa g1.fa -V 10 --init-state read-count full-em --depth-scheduler linear-gradient > g1_k16.tsv
+dbgphmm -k 32 benchmark g1_r1.fa g1.fa -V 10 --init-state read-count full-em --depth-scheduler linear-gradient > g1_k32.tsv
+
+dbgphmm -k 8 generate -l 200 > g2.fa
+dbgphmm -k 8 stat g2.fa > g2.json
+dbgphmm -k 120 sample g2.fa --length 1000 --n-reads 10 --start-from-head > g2_r1.fa
+dbgphmm -k 8 benchmark g2_r1.fa g2.fa -V 10 --init-state read-count full-em --depth-scheduler linear-gradient > g2_k8.tsv
+dbgphmm -k 16 benchmark g2_r1.fa g2.fa -V 10 --init-state read-count full-em --depth-scheduler linear-gradient > g2_k16.tsv
+dbgphmm -k 32 benchmark g2_r1.fa g2.fa -V 10 --init-state read-count full-em --depth-scheduler linear-gradient > g2_k32.tsv
--- scripts/kmer-freq.sh
+++ scripts/kmer-freq.sh
@@ -2,9 +2,18 @@
 # count-base
 # good?
 
-# dbgphmm -k 8 generate -l 300 > g1.fa
-dbgphmm -k 8 stat g1.fa > g1.json
-dbgphmm -k 400 sample g1.fa --length 1000 --n-reads 10 --start-from-head > g1_r1.fa
-# dbgphmm -k 8 --p-mismatch 0 --p-gap-open 0 --p-gap-ext 0 --p-end 0.0 sample g1.fa --length 10000 --n-reads 10 --start-from-head > g1_r2.fa
+cd data/kmer-freq
 
-# dbgphmm -k 8 benchmark g1_r1.fa g1.fa -V 10 --init-state zero freq-em
+dbgphmm -k 8 generate -l 50 > g1.fa
+dbgphmm -k 8 stat g1.fa > g1.json
+dbgphmm -k 30 sample g1.fa --length 1000 --n-reads 10 --start-from-head > g1_r1.fa
+dbgphmm -k 8 benchmark g1_r1.fa g1.fa -V 10 --init-state zero freq-em > g1_k8.tsv
+dbgphmm -k 16 benchmark g1_r1.fa g1.fa -V 10 --init-state zero freq-em > g1_k16.tsv
+dbgphmm -k 32 benchmark g1_r1.fa g1.fa -V 10 --init-state zero freq-em > g1_k32.tsv
+
+dbgphmm -k 8 generate -l 200 > g2.fa
+dbgphmm -k 8 stat g2.fa > g2.json
+dbgphmm -k 120 sample g2.fa --length 1000 --n-reads 10 --start-from-head > g2_r1.fa
+dbgphmm -k 8 benchmark g2_r1.fa g2.fa -V 10 --init-state zero freq-em > g2_k8.tsv
+dbgphmm -k 16 benchmark g2_r1.fa g2.fa -V 10 --init-state zero freq-em > g2_k16.tsv
+dbgphmm -k 32 benchmark g2_r1.fa g2.fa -V 10 --init-state zero freq-em > g2_k32.tsv
