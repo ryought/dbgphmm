be7a06a49bfc4136117d47d68fac793032a19066
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2021-10-29 11:04:56 +0900

    add forward_plotter

---
+++ scripts/forward_plotter.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+"""
+import argparse
+import json
+import csv
+from dataclasses import dataclass
+import numpy as np
+import matplotlib
+# matplotlib.use("TkAgg")
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from scipy.special import logsumexp
+
+def parse_matrix(tsv_filename):
+    kmers = []
+    probs = []
+    with open(tsv_filename) as f:
+        reader = csv.reader(f, delimiter='\t')
+        for row in reader:
+            kmers.append(row[0])
+            probs.append(list(map(lambda e: float(e), row[1:])))
+    probs = np.array(probs)
+    return kmers, probs
+
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('tsv_filename', type=str, help='tsv created by dbgphmm forward')
+    args = parser.parse_args()
+
+    # parse
+    kmers, probs = parse_matrix(args.tsv_filename)
+    n_kmer, n_bases = probs.shape
+
+    for y in range(n_bases):
+        probs[:, y] = probs[:, y] - logsumexp(probs[:, y])
+
+    plt.figure(figsize=(30, 8))
+    plt.imshow(np.exp(probs.T))
+    # plt.imshow(probs.T)
+
+    plt.savefig(args.tsv_filename + '.png', dpi=200)
+
+if __name__ == '__main__':
+    main()
