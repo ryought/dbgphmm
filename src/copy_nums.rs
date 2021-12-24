6cabcb1f8e7e4c15a5a7b37370ef519b545de4f9
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2021-12-24 13:56:39 +0900

    add edge_consistency check for ecdbg

--- src/copy_nums.rs
+++ src/copy_nums.rs
@@ -59,7 +59,9 @@ impl<'a> Ncdbg<'a> {
         cdbg.is_consistent_copy_num(&copy_nums.0)
     }
     pub fn copy_num(&self, v: &Node) -> u32 {
-        // TODO assert v is valid node
+        if !self.cdbg.is_valid_node(v) {
+            panic!("node {:?} is not in cdbg", v);
+        }
         self.copy_nums[v]
     }
 }
@@ -77,39 +79,59 @@ impl<'a> Ecdbg<'a> {
         node_copy_nums: CopyNums,
         edge_copy_nums: EdgeCopyNums,
     ) -> Ecdbg {
-        assert!(Ecdbg::is_node_consistent(cdbg, &node_copy_nums));
-        assert!(Ecdbg::is_edge_consistent(
-            cdbg,
-            &node_copy_nums,
-            &edge_copy_nums
-        ));
         Ecdbg {
             cdbg,
             node_copy_nums,
             edge_copy_nums,
         }
     }
-    fn is_node_consistent(cdbg: &CompressedDBG, node_copy_nums: &CopyNums) -> bool {
-        cdbg.is_consistent_copy_num(&node_copy_nums.0)
-    }
-    fn is_edge_consistent(_: &CompressedDBG, _: &CopyNums, _: &EdgeCopyNums) -> bool {
-        // TODO
-        true
-    }
     fn copy_num(&self, v: &Node) -> u32 {
+        if !self.cdbg.is_valid_node(v) {
+            panic!("node {:?} is not in cdbg", v);
+        }
         self.node_copy_nums[v]
     }
-    /*
     fn edge_copy_num(&self, v: &Node, w: &Node) -> u32 {
         if !self.cdbg.is_adjacent(v, w) {
             panic!("edge (v,w) = ({:?},{:?}) is not in cdbg", v, w);
         } else {
             let i = self.cdbg.childs(v).iter().position(|u| u == w).unwrap();
-            self.edge_copy_nums[]
+            self.edge_copy_nums[&Edgei::new(*v, i)]
         }
-        0
     }
-    */
+    fn is_node_consistent(&self) -> bool {
+        self.cdbg.is_consistent_copy_num(&self.node_copy_nums.0)
+    }
+    /// check that edge_{child,parent}_consistency
+    /// for all nodes
+    fn is_edge_consistent(&self) -> bool {
+        for v in self.cdbg.iter_nodes() {
+            if !self.is_edge_child_consistent(&v) || !self.is_edge_parent_consistent(&v) {
+                return false;
+            }
+        }
+        true
+    }
+    fn is_edge_child_consistent(&self, v: &Node) -> bool {
+        let sum_copy_number_of_childs: u32 = self
+            .cdbg
+            .childs(v)
+            .iter()
+            .map(|w| self.edge_copy_num(v, w))
+            .sum();
+        let my_copy_number: u32 = self.copy_num(v);
+        my_copy_number == sum_copy_number_of_childs
+    }
+    fn is_edge_parent_consistent(&self, v: &Node) -> bool {
+        let sum_copy_number_of_parents: u32 = self
+            .cdbg
+            .parents(v)
+            .iter()
+            .map(|w| self.edge_copy_num(w, v))
+            .sum();
+        let my_copy_number: u32 = self.copy_num(v);
+        my_copy_number == sum_copy_number_of_parents
+    }
 }
 
 #[cfg(test)]
@@ -122,5 +144,17 @@ mod tests {
         println!("n={}, c={}", cdbg.n_kmers(), cdbg.n_cycles());
         let ncdbg = Ncdbg::new(&cdbg, CopyNums(copy_nums));
         println!("{}", ncdbg.copy_num(&Node(0)));
+    }
+    #[test]
+    fn ecdbg_0() {
+        let (cdbg, copy_nums) = test_cdbg_01();
+        // TODO separate this creation for random some valid edge weights
+        let edge_copy_nums: Vec<Vec<u32>> = cdbg
+            .iter_nodes()
+            .map(|v| cdbg.childs(&v).iter().map(|_| 1).collect())
+            .collect();
+        let ecdbg = Ecdbg::new(&cdbg, CopyNums(copy_nums), EdgeCopyNums(edge_copy_nums));
+        println!("{}", ecdbg.is_node_consistent());
+        println!("{}", ecdbg.is_edge_consistent());
     }
 }
