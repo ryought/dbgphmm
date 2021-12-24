729f86c0c3f9bb25efa641d25c9d6ec69e7b2bd6
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2021-12-24 11:36:53 +0900

    ncdbg as phmm

--- src/copy_nums.rs
+++ src/copy_nums.rs
@@ -1,32 +1,44 @@
 use crate::compressed_dbg::CompressedDBG;
 
 // node copy-num labeled
-struct Ncdbg<'a> {
-    cdbg: &'a CompressedDBG,
-    copy_nums: Vec<u32>,
+pub struct Ncdbg<'a> {
+    pub cdbg: &'a CompressedDBG,
+    pub copy_nums: Vec<u32>,
 }
 
 impl<'a> Ncdbg<'a> {
     pub fn new(cdbg: &CompressedDBG, copy_nums: Vec<u32>) -> Ncdbg {
+        assert!(Ncdbg::is_consistent(cdbg, &copy_nums));
         Ncdbg { cdbg, copy_nums }
+    }
+    fn is_consistent(cdbg: &CompressedDBG, copy_nums: &[u32]) -> bool {
+        cdbg.is_consistent_copy_num(copy_nums)
     }
 }
 
 // edge copy-num labeled
-struct Ecdbg<'a> {
-    cdbg: &'a CompressedDBG,
-    node_copy_nums: Vec<u32>,
-    edge_copy_nums: Vec<u32>,
+pub struct Ecdbg<'a> {
+    pub cdbg: &'a CompressedDBG,
+    pub node_copy_nums: Vec<u32>,
+    pub edge_copy_nums: Vec<u32>,
 }
 
 impl<'a> Ecdbg<'a> {
     pub fn new(cdbg: &CompressedDBG, node_copy_nums: Vec<u32>, edge_copy_nums: Vec<u32>) -> Ecdbg {
-        // TODO assert edge copy num is consistent to node copy num
+        assert!(Ecdbg::is_node_consistent(cdbg, &node_copy_nums));
+        assert!(Ecdbg::is_edge_consistent());
         Ecdbg {
             cdbg,
             node_copy_nums,
             edge_copy_nums,
         }
+    }
+    fn is_node_consistent(cdbg: &CompressedDBG, node_copy_nums: &[u32]) -> bool {
+        cdbg.is_consistent_copy_num(node_copy_nums)
+    }
+    fn is_edge_consistent() -> bool {
+        // TODO
+        true
     }
 }
 
@@ -36,7 +48,8 @@ mod tests {
     use crate::mocks::test_cdbg_01;
     #[test]
     fn ncdbg_0() {
-        let (cdbg, _) = test_cdbg_01();
-        println!("n={}, c={}", cdbg.n_kmers(), cdbg.n_cycles())
+        let (cdbg, copy_nums) = test_cdbg_01();
+        println!("n={}, c={}", cdbg.n_kmers(), cdbg.n_cycles());
+        let ncdbg = Ncdbg::new(&cdbg, copy_nums);
     }
 }
--- src/hmm/copy_nums.rs
+++ src/hmm/copy_nums.rs
@@ -1,5 +1,6 @@
 use super::base::{Node, PHMM};
 use crate::copy_nums::{Ecdbg, Ncdbg};
+use crate::prob::Prob;
 
 impl<'a> PHMM for Ncdbg<'a> {
     fn n_nodes(&self) -> usize {
@@ -18,28 +19,29 @@ impl<'a> PHMM for Ncdbg<'a> {
         self.copy_nums[v.0] as f64
     }
     fn total_copy_num(&self) -> f64 {
-        self.copy_nums.iter().sum::<f64>()
+        // FIXME
+        // self.copy_nums.iter().sum::<f64>()
+        0f64
     }
     fn emission(&self, v: &Node) -> u8 {
         self.cdbg.emission(v)
     }
     /// a_vw = c_w / sum of c_w' for w' in childs of v
-    fn trans_prob(&self, v: &Node, w: &Node) -> Prob {}
     fn trans_prob(&self, v: &Node, w: &Node) -> Prob {
-        match self.cdbg.childs(v).iter().position(|&i| i == *w) {
-            Some(index) => self.trans_probs[v.0][index],
-            None => Prob::from_prob(0.0),
-        }
+        // FIXME
+        Prob::from_prob(0.0)
     }
     fn label(&self, v: &Node) -> String {
         self.cdbg.kmer(v).to_string()
     }
 }
 
+/*
 impl<'a> PHMM for Ecdbg<'a> {
     /// a_vw = c_vw / c_v
     fn trans_prob(&self, v: &Node, w: &Node) -> Prob {}
 }
+*/
 
 #[cfg(test)]
 mod tests {
