cedead84cd1a2111b41f5ef404b3433b43d7a5a2
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-01-29 22:55:44 +0900

    add docs

--- src/kmer/common.rs
+++ src/kmer/common.rs
@@ -4,27 +4,90 @@
 use super::kmer::Kmer;
 
 pub trait KmerLike: std::marker::Sized {
+    ///
+    /// k of the k-mer
+    ///
     fn len(&self) -> usize;
+    ///
+    /// k of the k-mer
+    /// (an alias of KmerLike.len)
+    ///
     fn k(&self) -> usize {
         self.len()
     }
-    fn adjacent(&self, other: &Kmer) -> bool;
+    ///
+    /// ABBBB -> A
+    ///
     fn first(&self) -> u8;
+    ///
+    /// AAAAB -> B
+    ///
     fn last(&self) -> u8;
+    ///
+    /// prefix of the kmer
+    /// AAAAB -> AAAA
+    ///
     fn prefix(&self) -> Kmer;
+    ///
+    /// suffix of the kmer
+    /// ABBBB -> BBBB
+    ///
     fn suffix(&self) -> Kmer;
+    ///
+    /// ABBBB and BBBBC is adjacent
+    ///
+    fn adjacent(&self, other: &Kmer) -> bool;
+    // TODO default implementation
+    // fn adjacent(&self, other: &Kmer) -> bool {
+    //     self.suffix() == other.prefix()
+    // }
+    ///
+    /// XYYYY -> [YYYYA, YYYYC, YYYYG, YYYYT]
+    ///
     fn childs(&self) -> Vec<Kmer>;
+    ///
+    /// YYYYZ -> [AYYYY, CYYYY, GYYYY, TYYYY]
+    ///
     fn parents(&self) -> Vec<Kmer>;
+    ///
+    /// union of childs and parents
+    /// XYYYZ -> [
+    ///            YYYZA, YYYZC, YYYZG, YYYZT, (childs)
+    ///            AXYYY, CXYYY, GXYYY, TXYYY  (parents)
+    ///          ]
+    ///
     fn neighbors(&self) -> Vec<Kmer>;
+    ///
+    /// XX (k mer) -> [AXX, CXX, GXX, TXX] (k+1 mer)
+    ///
     fn preds(&self) -> Vec<Kmer>;
+    ///
+    /// XX (k mer) -> [XXA, XXC, XXG, XXT] (k+1 mer)
+    ///
     fn succs(&self) -> Vec<Kmer>;
+    ///
+    /// (XYYY, YYYZ) (two k mers) -> XYYYZ (k+1 mer)
+    ///
+    fn join(&self, other: &Kmer) -> Kmer;
+    ///
+    /// head <==> NNNNX
+    ///
+    fn is_head(&self) -> bool;
+    ///
+    /// tail <==> XNNNN
+    ///
+    fn is_tail(&self) -> bool;
+    ///
+    /// last base is not N
+    ///
+    fn is_emitable(&self) -> bool;
+    ///
+    /// first base is N
+    ///
+    fn is_starting(&self) -> bool;
+    // internal functions
     fn extend_first(&self, first_base: u8) -> Kmer;
     fn extend_last(&self, last_base: u8) -> Kmer;
-    fn join(&self, other: &Kmer) -> Kmer;
-    fn is_head(&self) -> bool;
-    fn is_tail(&self) -> bool;
-    fn is_emitable(&self) -> bool;
-    fn is_starting(&self) -> bool;
     // construction
     // fn from(bases: &[u8]) -> Self;
     // fn to_vec(&self) -> Vec<u8>;
