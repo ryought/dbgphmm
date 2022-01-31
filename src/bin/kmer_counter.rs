3302ede4f050f17659937b2428bd01324fed4970
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-01-31 11:59:05 +0900

    fix kmer_counter

--- src/bin/kmer_counter.rs
+++ src/bin/kmer_counter.rs
@@ -4,7 +4,8 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let k: usize = args[1].to_string().parse().unwrap();
-    let kmer = TinyKmer::from(b"ATCGGGAT");
-    println!("k={} kmer={}", k, kmer);
+    let _k: usize = args[1].to_string().parse().unwrap();
+    // TODO errors
+    // let kmer = TinyKmer::from(b"ATCGGGAT");
+    // println!("k={} kmer={}", k, kmer);
 }
