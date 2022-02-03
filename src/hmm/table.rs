31af3b47265068eed05fb60c320ab2ab405987be
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-02-03 14:35:47 +0900

    use NodeVec in PHMMTable

--- src/hmm/table.rs
+++ src/hmm/table.rs
@@ -7,6 +7,7 @@
 //!
 //! F[Match,v] or B[Match,v]
 //!
+use super::veclikewrap::NodeVec;
 use crate::prob::Prob;
 use crate::veclike::{DenseVec, VecLike};
 
@@ -18,11 +19,11 @@ pub const MAX_DEL: usize = 4;
 #[derive(Debug, Clone)]
 pub struct PHMMTable<V: VecLike<Prob>> {
     /// Match node probability
-    pub m: V,
+    pub m: NodeVec<V>,
     /// Ins node probability
-    pub i: V,
+    pub i: NodeVec<V>,
     /// Del node probability
-    pub d: V,
+    pub d: NodeVec<V>,
     /// Match node in begin state
     pub mb: Prob,
     /// Ins node in begin state
@@ -31,4 +32,22 @@ pub struct PHMMTable<V: VecLike<Prob>> {
     pub e: Prob,
 }
 
+impl<V: VecLike<Prob>> PHMMTable<V> {
+    pub fn new(n_nodes: usize, m: Prob, i: Prob, d: Prob, mb: Prob, ib: Prob, e: Prob) -> Self {
+        PHMMTable {
+            m: NodeVec::new(n_nodes, m),
+            i: NodeVec::new(n_nodes, i),
+            d: NodeVec::new(n_nodes, d),
+            mb,
+            ib,
+            e,
+        }
+    }
+}
+
 // impl PHMMForward for PHMM {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
