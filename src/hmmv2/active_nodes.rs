523a8c384ae0e34fb826c4baafc9adff7ff4797d
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-02-17 10:36:37 +0900

    add itertools k_smallest test

--- src/hmmv2/active_nodes.rs
+++ src/hmmv2/active_nodes.rs
@@ -50,13 +50,40 @@ impl ActiveNodes {
     where
         S: Storage<Item = Prob>,
     {
-        unimplemented!();
+        self.fit_to_nodevec(&t.to_nodevec(), n_active_nodes)
     }
+    ///
+    /// Create active_nodes list from NodeVec
+    /// by taking the n_active_nodes highest prob nodes from `v`.
     ///
     pub fn fit_to_nodevec<S>(&self, v: &NodeVec<S>, n_active_nodes: usize) -> ActiveNodes
     where
         S: Storage<Item = Prob>,
     {
+        // v.iter().k_smallest()
         unimplemented!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::common::ni;
+    use crate::prob::p;
+
+    #[test]
+    fn itertools_k_smallest() {
+        let xs = vec![(p(0.2), ni(0)), (p(0.1), ni(10)), (p(0.5), ni(3))];
+        // let sm = xs.iter().k_smallest(2);
+        // k_largest
+        let ys: Vec<NodeIndex> = xs
+            .iter()
+            .sorted_by(|a, b| Ord::cmp(&b, &a))
+            .take(2)
+            .map(|(_, v)| v)
+            .copied()
+            .collect();
+        println!("{:?}", ys);
+        assert_eq!(ys, vec![ni(3), ni(0)])
     }
 }
