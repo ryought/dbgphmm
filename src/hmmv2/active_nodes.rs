43c55b1f97494f62099fe4aff07700e00e98fdbc
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-02-17 14:39:37 +0900

    add to_parents_and_us

--- src/hmmv2/active_nodes.rs
+++ src/hmmv2/active_nodes.rs
@@ -46,6 +46,23 @@ impl ActiveNodes {
         }
     }
     ///
+    /// create a new active nodes consists of our parent nodes and current nodes
+    ///
+    pub fn to_parents_and_us<N: PHMMNode, E: PHMMEdge>(&self, m: &PHMMModel<N, E>) -> ActiveNodes {
+        match self {
+            ActiveNodes::All => ActiveNodes::All,
+            ActiveNodes::Only(nodes) => {
+                let parents: Vec<NodeIndex> = nodes
+                    .iter()
+                    .flat_map(|&node| m.parents(node).map(|(_, parent, _)| parent))
+                    .chain(nodes.iter().copied())
+                    .unique()
+                    .collect();
+                ActiveNodes::Only(parents)
+            }
+        }
+    }
+    ///
     /// Create active_nodes list from NodeVec
     /// by taking the n_active_nodes highest prob nodes from `v`.
     ///
@@ -67,8 +84,28 @@ impl ActiveNodes {
 mod tests {
     use super::*;
     use crate::common::ni;
+    use crate::hmm::params::PHMMParams;
+    use crate::hmmv2::mocks::mock_linear_phmm;
     use crate::prob::p;
     use crate::vector::dense::DenseStorage;
+
+    #[test]
+    fn active_nodes_update() {
+        let phmm = mock_linear_phmm(PHMMParams::zero_error());
+        let an = ActiveNodes::Only(vec![ni(1)]);
+        assert_eq!(an.to_childs(&phmm), ActiveNodes::Only(vec![ni(2)]));
+        assert_eq!(an.to_parents(&phmm), ActiveNodes::Only(vec![ni(0)]));
+        assert_eq!(
+            an.to_parents_and_us(&phmm),
+            ActiveNodes::Only(vec![ni(0), ni(1)])
+        );
+
+        let an = ActiveNodes::Only(vec![ni(0)]);
+        assert_eq!(an.to_parents(&phmm), ActiveNodes::Only(vec![]));
+
+        let an = ActiveNodes::Only(vec![ni(3), ni(6), ni(6)]);
+        assert_eq!(an.to_parents(&phmm), ActiveNodes::Only(vec![ni(2), ni(5)]));
+    }
 
     #[test]
     fn active_nodes_from_node_vec() {
