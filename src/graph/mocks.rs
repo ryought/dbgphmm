f38c4fa2e408c9a70fa1223ab90f515f5fc29a67
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-02-17 12:09:04 +0900

    add randomly generated mocks

--- src/graph/mocks.rs
+++ src/graph/mocks.rs
@@ -1,4 +1,5 @@
 use super::genome_graph::{GenomeEdge, GenomeGraph, GenomeNode};
+use crate::random_seq::generate;
 use petgraph::graph::{DiGraph, EdgeIndex, NodeIndex};
 
 ///
@@ -10,6 +11,16 @@ pub fn mock_linear() -> GenomeGraph {
     GenomeGraph(g)
 }
 
+///
+/// Random linear genome with length and from seed
+///
+pub fn mock_linear_random(length: usize, seed: u64) -> GenomeGraph {
+    let seq = generate(length, seed);
+    let mut g = DiGraph::new();
+    g.add_node(GenomeNode::new(&seq, 1));
+    GenomeGraph(g)
+}
+
 pub fn mock() -> GenomeGraph {
     let mut g = DiGraph::new();
     let v1 = g.add_node(GenomeNode::new(b"ATTCGATCGTCG", 1));
@@ -18,4 +29,17 @@ pub fn mock() -> GenomeGraph {
     g.add_edge(v1, v3, GenomeEdge::new(None));
     g.add_edge(v2, v3, GenomeEdge::new(None));
     GenomeGraph(g)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mock_linear_random() {
+        let gg = mock_linear_random(100, 1);
+        println!("{}", gg);
+        assert_eq!(gg.node_count(), 1);
+        assert_eq!(gg.edge_count(), 0);
+    }
 }
