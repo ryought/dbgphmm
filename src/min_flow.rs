7b6a608cfe669ce1012605d49ee52907bf1ffe93
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-02-18 10:31:49 +0900

    FlowEdgeLike -> FlowEdge

--- src/min_flow.rs
+++ src/min_flow.rs
@@ -6,7 +6,7 @@ pub mod utils;
 pub mod zero_demand;
 
 use convex::{restore_convex_flow, to_fixed_flow_graph, ConvexFlowGraph};
-use flow::{is_valid_flow, Flow, FlowEdgeLike, FlowGraphRaw};
+use flow::{is_valid_flow, Flow, FlowEdge, FlowGraphRaw};
 use petgraph::graph::DiGraph;
 use residue::improve_flow;
 use utils::draw_with_flow;
@@ -22,7 +22,7 @@ use zero_demand::{find_initial_flow, is_zero_demand_flow_graph};
 pub fn min_cost_flow<N, E>(graph: &DiGraph<N, E>) -> Option<Flow>
 where
     N: std::fmt::Debug,
-    E: FlowEdgeLike + std::fmt::Debug,
+    E: FlowEdge + std::fmt::Debug,
 {
     let init_flow = find_initial_flow(graph);
 
@@ -61,7 +61,7 @@ pub fn min_cost_flow_convex(graph: &ConvexFlowGraph) -> Option<Flow> {
 ///
 /// Find minimum cost flow of the special FlowGraph, whose demand is always zero.
 ///
-fn min_cost_flow_from_zero<N, E: FlowEdgeLike>(graph: &DiGraph<N, E>) -> Flow {
+fn min_cost_flow_from_zero<N, E: FlowEdge>(graph: &DiGraph<N, E>) -> Flow {
     assert!(is_zero_demand_flow_graph(&graph));
     let flow = Flow::new(graph.edge_count(), 0);
     min_cost_flow_from(graph, &flow)
@@ -70,7 +70,7 @@ fn min_cost_flow_from_zero<N, E: FlowEdgeLike>(graph: &DiGraph<N, E>) -> Flow {
 ///
 /// Find minimum cost by starting from the specified flow values.
 ///
-fn min_cost_flow_from<N, E: FlowEdgeLike>(graph: &DiGraph<N, E>, init_flow: &Flow) -> Flow {
+fn min_cost_flow_from<N, E: FlowEdge>(graph: &DiGraph<N, E>, init_flow: &Flow) -> Flow {
     let mut flow = init_flow.clone();
 
     loop {
