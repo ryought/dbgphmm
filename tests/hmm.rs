cf281f9a33bac194063ef1045d93a837f108a823
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-02-22 10:33:48 +0900

    update test

--- tests/hmm.rs
+++ tests/hmm.rs
@@ -38,6 +38,12 @@ fn check_node_freq_similarity(phmm: &PModel, h: &History) {
     let nf_sparse = o_sparse.to_node_freqs();
     println!("{}", nf_sparse.sum());
 
+    /*
+    for (v, _) in phmm.nodes() {
+        println!("{:?}: dense - sparse={}", v, nf_dense[v] - nf_sparse[v]);
+    }
+    */
+
     // forward and backward is consistent in dense
     assert_abs_diff_eq!(
         o_dense.to_full_prob_forward(),
@@ -60,8 +66,8 @@ fn check_node_freq_similarity(phmm: &PModel, h: &History) {
         let sb = o_sparse.backward.table(i);
         let diff1 = df.diff(&sf);
         let diff2 = db.diff(&sb);
-        println!("forward_dense-forward_sparse = {}", diff1);
-        println!("backward_dense-backward_sparse = {}", diff1);
+        // println!("forward_dense-forward_sparse = {}", diff1);
+        // println!("backward_dense-backward_sparse = {}", diff1);
         assert!(diff1 < 0.000000001);
         assert!(diff2 < 0.000000001);
     }
@@ -72,6 +78,10 @@ fn check_node_freq_similarity(phmm: &PModel, h: &History) {
         o_sparse.to_full_prob_forward(),
         epsilon = 0.1
     );
+    println!("p_f[dense, forward]={}", o_dense.to_full_prob_forward());
+    println!("p_f[sparse, forward]={}", o_sparse.to_full_prob_forward());
+    println!("p_f[dense, backward]={}", o_dense.to_full_prob_backward());
+    println!("p_f[sparse, backward]={}", o_sparse.to_full_prob_backward());
 
     // assert all emit probs are similar between dense and sparse
     let epd: Vec<_> = o_dense.iter_emit_probs().collect();
@@ -104,21 +114,27 @@ fn check_node_freq_similarity(phmm: &PModel, h: &History) {
         }
         */
         println!("diff={}", diff);
-        // assert!(diff < 0.001);
+        assert!(diff < 0.001);
     }
 
     // assert node_freq between dense and sparse is similar
-    // assert_abs_diff_eq!(nf_dense.sum(), nf_sparse.sum(), epsilon = 0.01);
+    assert_abs_diff_eq!(nf_dense.sum(), nf_sparse.sum(), epsilon = 0.01);
 }
 
 #[test]
 #[ignore]
 fn hmmv2_linear_long_node_freq_similarity() {
-    let phmm = mock_linear_random_phmm(1000, 2, PHMMParams::default());
+    let mut param = PHMMParams::default();
+    param.n_warmup = 40;
+    // param.n_warmup = 20;
+    // param.n_active_nodes = 16;
+    // param.n_active_nodes = 32;
+    let phmm = mock_linear_random_phmm(1000, 2, param);
     println!("phmm created");
     let h = phmm.sample(500, 4);
     // let h = phmm.sample(500, 2);
-    println!("{}", h);
+    // let h = phmm.sample(500, 3);
+    // println!("{}", h);
     println!("sample created");
     check_node_freq_similarity(&phmm, &h);
 }
