9eb465d02bb02b0075fac0301aeb734499b9b2ac
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-02-22 10:38:24 +0900

    add test cases

--- tests/hmm.rs
+++ tests/hmm.rs
@@ -138,3 +138,12 @@ fn hmmv2_linear_long_node_freq_similarity() {
     println!("sample created");
     check_node_freq_similarity(&phmm, &h);
 }
+
+use test_case::test_case;
+#[test_case(-2, -4 ; "when both operands are negative")]
+#[test_case(2,  4  ; "when both operands are positive")]
+#[test_case(4,  2  ; "when operands are swapped")]
+fn multiplication_tests(x: i8, y: i8) {
+    let actual = (x * y).abs();
+    assert_eq!(8, actual)
+}
