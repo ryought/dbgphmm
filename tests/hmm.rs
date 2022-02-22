bd863de6996696c21d95cb82d638b45db68a02b7
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-02-22 11:02:40 +0900

    backward dense/sparse test with different n_warmup and n_active_nodes

--- tests/hmm.rs
+++ tests/hmm.rs
@@ -139,6 +139,43 @@ fn hmmv2_linear_long_node_freq_similarity() {
     check_node_freq_similarity(&phmm, &h);
 }
 
+#[test]
+fn hmmv2_backward_dense_and_sparse() {
+    let n_read_bases = 100;
+    let n_genome_bases = 1000;
+    let mut param = PHMMParams::default();
+    let mut phmm = mock_linear_random_phmm(n_genome_bases, 2, param);
+    for seed in 0..10 {
+        let h = phmm.sample(n_read_bases, seed);
+        let r = h.to_sequence();
+        for (n_warmup, n_active_nodes) in [(10, 10), (20, 10), (40, 10), (40, 40), (40, 80)] {
+            phmm.param.n_warmup = n_warmup;
+            phmm.param.n_active_nodes = n_active_nodes;
+            // println!("{}", h);
+            let dense = phmm.backward(&r);
+            let sparse = phmm.backward_sparse(&r);
+            for i in 0..dense.n_emissions() {
+                let td = dense.table(i);
+                let ts = sparse.table(i);
+                let d1 = td.diff(&ts);
+                let d2 = ts.diff(&td);
+                // |s-d| should be equal to |d-s|
+                assert_abs_diff_eq!(d1, d2);
+                // Since the last n_active_nodes tables will be calculated
+                // using same dense method, so the table should be exactly same.
+                if dense.n_emissions() - i < n_warmup {
+                    assert_eq!(d1, 0.0)
+                }
+            }
+            let d = dense.first_table().diff(&sparse.first_table());
+            println!(
+                "n_w={} n_a={} seed={} d={}",
+                n_warmup, n_active_nodes, seed, d
+            );
+        }
+    }
+}
+
 use test_case::test_case;
 #[test_case(-2, -4 ; "when both operands are negative")]
 #[test_case(2,  4  ; "when both operands are positive")]
