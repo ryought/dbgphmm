a9118922a6e03a871d8a89dc5de61b776b2a2e98
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-03-18 14:31:37 +0900

    mock of compression em

--- src/em/compression.rs
+++ src/em/compression.rs
@@ -9,17 +9,47 @@
 //!
 //! Solve min-flow and determine node copy number.
 //!
-use crate::dbg::dbg::{Dbg, DbgEdge, DbgNode};
-use crate::hmmv2::freq::Reads;
+use crate::dbg::dbg::{Dbg, DbgEdge, DbgNode, NodeCopyNums};
+use crate::hmmv2::freq::{NodeFreqs, Reads};
+use crate::hmmv2::params::PHMMParams;
 
 ///
 ///
 ///
-pub fn compression<N: DbgNode, E: DbgEdge>(dbg: &Dbg<N, E>, reads: &Reads) -> Dbg<N, E> {
+pub fn compression<N: DbgNode, E: DbgEdge>(
+    dbg: &Dbg<N, E>,
+    reads: &Reads,
+    size: usize,
+) -> Dbg<N, E> {
     // e-step
     // calculate node_freqs by using current dbg.
+    let params = PHMMParams::default();
+    let node_freqs = compression_e_step(dbg, reads, &params);
 
     // m-step
     // convert it to the
+    let copy_nums = compression_m_step(dbg, &node_freqs);
+    unimplemented!();
+}
+
+///
+/// E-step of compression
+///
+fn compression_e_step<N: DbgNode, E: DbgEdge>(
+    dbg: &Dbg<N, E>,
+    reads: &Reads,
+    params: &PHMMParams,
+) -> NodeFreqs {
+    let phmm = dbg.to_phmm(params.clone());
+    phmm.to_node_freqs(reads)
+}
+
+///
+/// M-step of compression
+///
+fn compression_m_step<N: DbgNode, E: DbgEdge>(
+    dbg: &Dbg<N, E>,
+    node_freqs: &NodeFreqs,
+) -> NodeCopyNums {
     unimplemented!();
 }
