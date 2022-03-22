5985bd074f9472805a7af9ef5d638933b1040f3d
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-03-22 15:35:30 +0900

    add extension mock

--- src/em/extension.rs
+++ src/em/extension.rs
@@ -7,9 +7,10 @@
 //! ## M-step
 //!
 //!
-use crate::dbg::dbg::{Dbg, DbgEdge, DbgNode};
+use crate::dbg::dbg::{Dbg, DbgEdge, DbgNode, EdgeCopyNums};
 use crate::hmmv2::freq::Reads;
 use crate::hmmv2::params::PHMMParams;
+use crate::hmmv2::trans_table::EdgeFreqs;
 
 ///
 /// Extension algorithm
@@ -20,7 +21,7 @@ use crate::hmmv2::params::PHMMParams;
 ///     Calculate edge_freqs on dbg.
 ///
 /// * m-step
-///     Maximize the score for each intersections
+///     Maximize the score for each intersections.
 ///
 /// ## TODOs
 ///
@@ -31,5 +32,42 @@ pub fn extension<N: DbgNode, E: DbgEdge>(
     reads: &Reads,
     params: &PHMMParams,
 ) -> Dbg<N, E> {
+    unimplemented!();
+}
+
+///
+/// E-step of extension
+///
+/// ## Details
+///
+/// * convert dbg into phmm.
+/// * calculate edge frequencies by forward/backward algorithm to emit the reads.
+///
+fn extension_e_step<N: DbgNode, E: DbgEdge>(
+    dbg: &Dbg<N, E>,
+    reads: &Reads,
+    params: &PHMMParams,
+) -> EdgeFreqs {
+    let phmm = dbg.to_phmm(params.clone());
+    phmm.to_edge_freqs(reads)
+}
+
+///
+/// M-step of extension
+///
+/// ## Params
+///
+/// * `dbg`
+///     de bruijn graph
+/// * `edge_freqs`
+///     edge usage frequencies.
+///
+/// ## Details
+///
+///
+fn extension_m_step<N: DbgNode, E: DbgEdge>(
+    dbg: &Dbg<N, E>,
+    edge_freqs: &EdgeFreqs,
+) -> EdgeCopyNums {
     unimplemented!();
 }
