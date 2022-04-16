ddaf3ac5e99d197ecd13dd168ea40eb392ef4e7e
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-04-16 11:05:46 +0900

    add scheduler

--- src/em/scheduler.rs
+++ src/em/scheduler.rs
@@ -7,15 +7,16 @@ use crate::common::Freq;
 ///
 /// EM two task
 ///
+#[derive(Clone, Debug)]
 pub enum Task {
     ///
     /// Compression to the specified depth
     ///
     Compression(Freq),
     ///
-    /// Run extension
+    /// Run extension to specified k-mer length
     ///
-    Extension,
+    Extension(usize),
 }
 
 ///
@@ -23,6 +24,7 @@ pub enum Task {
 /// a proper task to the iteration.
 ///
 pub trait Scheduler {
+    fn n_tasks(&self) -> usize;
     fn task(&self, iteration: usize) -> Option<Task>;
 }
 
@@ -33,6 +35,7 @@ pub trait Scheduler {
 ///
 /// type-1 scheduler
 ///
+#[derive(Clone, Debug)]
 pub struct SchedulerType1 {
     /// initial k-mer size
     k_init: usize,
@@ -40,21 +43,75 @@ pub struct SchedulerType1 {
     k_target: usize,
     /// true depth
     true_depth: Freq,
+    //
+    // internal variables
+    //
+    n_extension: usize,
+    n_compression: usize,
+    n_iteration: usize,
+    compression_interval: usize,
 }
 
 impl SchedulerType1 {
     pub fn new(k_init: usize, k_target: usize, true_depth: Freq) -> Self {
+        assert!(k_target >= k_init);
+        assert!(true_depth > 1.0);
+
+        // (1) compute n_extension and n_compression.
+        // how many times extension and compression should be executed?
+        let n_extension = k_target - k_init;
+        let n_compression = (true_depth - 1.0).ceil() as usize;
+        let n_iteration = n_extension + n_compression;
+        let compression_interval = if n_compression > 0 {
+            n_extension / n_compression
+        } else {
+            0
+        } + 1;
+
         SchedulerType1 {
             k_init,
             k_target,
             true_depth,
+            n_extension,
+            n_compression,
+            n_iteration,
+            compression_interval,
         }
     }
 }
 
 impl Scheduler for SchedulerType1 {
+    fn n_tasks(&self) -> usize {
+        self.n_iteration
+    }
     fn task(&self, iteration: usize) -> Option<Task> {
-        unimplemented!();
+        let stage = iteration / self.compression_interval;
+        let step_in_stage = iteration % self.compression_interval;
+        let is_final_stage =
+            (self.n_iteration - stage * self.compression_interval) < self.compression_interval;
+        println!(
+            "stage={} step_in_stage={} is_final_stage={}",
+            stage, step_in_stage, is_final_stage
+        );
+
+        if iteration < self.n_iteration {
+            if !is_final_stage {
+                if step_in_stage == 0 {
+                    // do compression
+                    let depth = (2.0 + stage as Freq).min(self.true_depth);
+                    Some(Task::Compression(depth))
+                } else {
+                    // do extension
+                    let k = self.k_init + (iteration - stage);
+                    Some(Task::Extension(k))
+                }
+            } else {
+                let k = self.k_init + (iteration - stage) + 1;
+                Some(Task::Extension(k))
+            }
+        } else {
+            None
+        }
     }
 }
 
@@ -66,5 +123,10 @@ impl Scheduler for SchedulerType1 {
 mod tests {
     use super::*;
     #[test]
-    fn starting() {}
+    fn scheduler_type1() {
+        let s = SchedulerType1::new(16, 17, 1.5);
+        for i in 0..s.n_tasks() {
+            println!("i={} task={:?}", i, s.task(i));
+        }
+    }
 }
