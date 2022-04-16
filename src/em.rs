740958fe3427c0bb1060c22e1bf9f46d4065431a
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-04-16 11:29:39 +0900

    add infer em function

--- src/em.rs
+++ src/em.rs
@@ -2,3 +2,35 @@ pub mod compression;
 pub mod e2e;
 pub mod extension;
 pub mod scheduler;
+use crate::common::{Freq, Reads};
+use crate::dbg::dbg::{Dbg, DbgEdge, DbgNode, EdgeCopyNums};
+use crate::hmmv2::params::PHMMParams;
+pub use scheduler::SchedulerType1;
+use scheduler::{Scheduler, Task};
+
+///
+/// Do EM inference of Dbg.
+///
+pub fn infer<N: DbgNode, E: DbgEdge, S: Scheduler>(
+    dbg: &Dbg<N, E>,
+    reads: &Reads,
+    params: &PHMMParams,
+    scheduler: &S,
+    max_iter: usize,
+) -> Dbg<N, E> {
+    let mut dbg = dbg.clone();
+
+    for iteration in 0..scheduler.n_tasks() {
+        let task = scheduler.task(iteration).unwrap();
+        match task {
+            Task::Compression(depth) => {
+                dbg = compression::compression(&dbg, reads, params, depth, max_iter);
+            }
+            Task::Extension(k) => {
+                dbg = extension::extension(&dbg, reads, params, max_iter);
+            }
+        }
+    }
+
+    dbg
+}
