12f37b5385e9dd1115285dc244e77819e61acdc4
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-05-25 14:39:23 +0900

    separate is_increasing and is_convex

--- src/min_flow/utils.rs
+++ src/min_flow/utils.rs
@@ -17,9 +17,16 @@ use petgraph::EdgeType;
 /// for increasing `x`s
 ///
 pub fn is_convex<F: Fn(usize) -> f64>(f: F, x_min: usize, x_max: usize) -> bool {
+    is_increasing(|x| f(x + 1) - f(x), x_min, x_max)
+}
+
+///
+/// check if the function `f` is monotonically increasing in the domain `[x_min, x_max]`.
+///
+pub fn is_increasing<F: Fn(usize) -> f64>(f: F, x_min: usize, x_max: usize) -> bool {
     let mut y_prev = f64::MIN;
 
-    (x_min..x_max).map(|x| f(x + 1) - f(x)).all(|y| {
+    (x_min..x_max).map(|x| f(x)).all(|y| {
         // check if ys are increasing
         let is_increasing = y >= y_prev;
         y_prev = y;
@@ -85,9 +92,9 @@ mod tests {
 
     #[test]
     fn is_convex_test() {
-        // f(x) = (x-10)^2
+        // f(x) = (x-10)^2 is convex
         assert!(is_convex(|x| (x as f64 - 10.0).powi(2), 0, 20));
-        // f(x) = - (x-10)^2
+        // f(x) = - (x-10)^2 is not convex
         assert!(!is_convex(|x| -(x as f64 - 10.0).powi(2), 0, 20));
         // f(x) = 0 (constant)
         assert!(is_convex(|_| 0.0, 0, 20));
@@ -95,5 +102,17 @@ mod tests {
         assert!(is_convex(|x| -10.0 * (x as f64).ln(), 1, 20));
         // f(x) = -c clamped_log(x)
         assert!(is_convex(|x| -10.0 * clamped_log(x), 0, 20));
+        // f(x) = 10x
+        assert!(is_convex(|x| 10.0 * x as f64, 0, 20));
+        // f(x) = -10x
+        assert!(is_convex(|x| -10.0 * x as f64, 0, 20));
+        // f(x) = 0x
+        assert!(is_convex(|x| 0.0 * x as f64, 0, 20));
+        // TODO f(x) = 0.0001x
+        // because of floating point calculation
+        // some function (with small number) fails this convexity test.
+        // assert!(is_convex(|x| 0.0001 * x as f64, 0, 20));
+        // but by analytically solving f(x+1)-f(x) it passes the test.
+        assert!(is_increasing(|_| 0.0001, 0, 20));
     }
 }
