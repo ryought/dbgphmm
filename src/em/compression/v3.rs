11b76f4c83eafffe6c146f59b8379cdaa195a6c3
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-06-02 11:43:11 +0900

    add display to V3

--- src/em/compression/v3.rs
+++ src/em/compression/v3.rs
@@ -33,6 +33,19 @@ pub enum Diff {
     Nop,
 }
 
+impl Diff {
+    ///
+    /// Apply the diff operation
+    ///
+    pub fn apply(&self, value: usize) -> usize {
+        match self {
+            Diff::Inc => value.saturating_add(1),
+            Diff::Dec => value.saturating_sub(1),
+            Diff::Nop => value,
+        }
+    }
+}
+
 impl CompressionV3KmerInfo {
     ///
     /// Current copy number of the kmer
@@ -57,45 +70,54 @@ impl CompressionV3KmerInfo {
     /// `X_l log (copy_num)` term
     ///
     pub fn x(&self, diff: Diff) -> f64 {
-        let new_copy_num = match diff {
-            Diff::Inc => self.0.copy_num + 1,
-            Diff::Dec => self.0.copy_num - 1,
-            Diff::Nop => self.0.copy_num,
-        };
+        let new_copy_num = diff.apply(self.0.copy_num);
         self.0.freq * clamped_log(new_copy_num)
     }
     ///
     /// `Y_i log (copy_num_intersection)` term
     ///
     pub fn y(&self, diff: Diff) -> f64 {
-        let new_copy_num_intersection = match diff {
-            Diff::Inc => self.0.copy_num_intersection + 1,
-            Diff::Dec => self.0.copy_num_intersection - 1,
-            Diff::Nop => self.0.copy_num_intersection,
-        };
+        let new_copy_num_intersection = diff.apply(self.0.copy_num_intersection);
         -self.0.freq_intersection * clamped_log(new_copy_num_intersection)
     }
     ///
     /// `Z log (copy_num_total)` term
     ///
     pub fn z(&self, diff: Diff) -> f64 {
-        let new_copy_num_total = match diff {
-            Diff::Inc => self.0.copy_num_total + 1,
-            Diff::Dec => self.0.copy_num_total - 1,
-            Diff::Nop => self.0.copy_num_total,
-        };
+        let new_copy_num_total = diff.apply(self.0.copy_num_total);
         -self.0.freq_init * clamped_log(new_copy_num_total)
     }
     ///
     /// regularization term `R`
     ///
     pub fn r(&self, diff: Diff) -> f64 {
-        let new_copy_num_total = match diff {
-            Diff::Inc => self.0.copy_num_total + 1,
-            Diff::Dec => self.0.copy_num_total - 1,
-            Diff::Nop => self.0.copy_num_total,
-        };
+        let new_copy_num_total = diff.apply(self.0.copy_num_total);
         -self.0.penalty_weight * (new_copy_num_total - self.0.copy_num_total_expected).pow(2) as f64
+    }
+}
+
+impl std::convert::From<KmerInfo> for CompressionV3KmerInfo {
+    fn from(kmer_info: KmerInfo) -> Self {
+        CompressionV3KmerInfo(kmer_info)
+    }
+}
+
+impl std::fmt::Display for CompressionV3KmerInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "{} 0={},{},{} +1={},{},{} -1={},{},{}",
+            self.0,
+            self.x(Diff::Nop),
+            self.y(Diff::Nop),
+            self.z(Diff::Nop),
+            self.x(Diff::Inc),
+            self.y(Diff::Inc),
+            self.z(Diff::Inc),
+            self.x(Diff::Dec),
+            self.y(Diff::Dec),
+            self.z(Diff::Dec),
+        )
     }
 }
 
@@ -121,6 +143,24 @@ impl<K: KmerLike> ConvexCost for SimpleEDbgEdgeWithV3KmerInfos<K> {
     fn convex_cost(&self, flow: usize) -> f64 {
         self.attribute.score(flow)
     }
+}
+
+///
+/// create plain KmerInfos and convert it to V3KmerInfos
+///
+fn create_kmer_infos<N: DbgNode, E: DbgEdge>(
+    dbg: &Dbg<N, E>,
+    edge_freqs: &EdgeFreqs,
+    init_freqs: &NodeFreqs,
+    genome_size: CopyNum,
+    penalty_weight: f64,
+) -> V3KmerInfos {
+    let mut ki = V3KmerInfos::new(dbg.n_nodes(), CompressionV3KmerInfo::default());
+    let ki0 = create_plain_kmer_infos(dbg, edge_freqs, init_freqs, genome_size, penalty_weight);
+    for (node, _) in dbg.nodes() {
+        ki[node] = CompressionV3KmerInfo::from(ki0[node]);
+    }
+    ki
 }
 
 // ** m-step **
