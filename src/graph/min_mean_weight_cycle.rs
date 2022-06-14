5d312694140f6661c4ae176e28205d8a54c08761
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-06-15 08:26:22 +0900

    merged backless into basic mmwc

--- src/graph/min_mean_weight_cycle.rs
+++ src/graph/min_mean_weight_cycle.rs
@@ -11,6 +11,10 @@ use super::FloatWeight;
 use petgraph::prelude::*;
 use petgraph::visit::{VisitMap, Visitable};
 
+//
+// basic shortest paths with node indexing
+//
+
 ///
 /// F[k][v] = (min weight path (with length k) from source to a node v)
 /// for k=0..n and all nodes v (reachable from source node) n=|V|
@@ -24,13 +28,13 @@ pub struct ShortestPaths {
     ///
     /// `dists[k: length of path][v: node]`
     ///
-    pub dists: Vec<Vec<f64>>,
+    dists: Vec<Vec<f64>>,
     ///
     /// Predecessors for backtracking
     /// `preds[k: length of path][v: node] = (w: node, e: edge)`
     /// means that "min weight path `F[k][v]` ends with the `e=w->v` edge"
     ///
-    pub preds: Vec<Vec<Option<(NodeIndex, EdgeIndex)>>>,
+    preds: Vec<Vec<Option<(NodeIndex, EdgeIndex)>>>,
 }
 
 impl ShortestPaths {}
@@ -70,6 +74,113 @@ pub fn shortest_paths<N, E: FloatWeight>(
     }
 
     ShortestPaths { dists, preds }
+}
+
+//
+// custom shortest paths with edge indexing for using edge-adjacency condition
+//
+
+///
+/// F[k][e] = (min weight path s.t.
+///   * length is `k`
+///   * ends with edge `e`
+///   * from source to a target node of `e`)
+///
+/// for `k=1..n` and all edges `e` (reachable from source node)
+/// (Here `n=|V|` because path length >= `n` means node repetition, so it should have a cycle)
+///
+/// and backtracking information
+///
+#[derive(Clone, Debug)]
+pub struct ShortestPathsByEdge {
+    ///
+    /// Distances
+    ///
+    /// `dists[k: length of path][e: edge]`
+    ///
+    dists: Vec<Vec<f64>>,
+    ///
+    /// Predecessors for backtracking
+    /// `preds[k: length of path][e: edge] = e': edge`
+    /// means that "min weight path `F[k][e]` ends with edges `(...,e',e)`"
+    ///
+    preds: Vec<Vec<Option<EdgeIndex>>>,
+}
+
+impl ShortestPathsByEdge {
+    ///
+    /// Convert edge-indexed ShortestPathsByEdge into
+    /// node-indexed ShortestPaths.
+    ///
+    pub fn into_shortest_paths<N, E>(self, graph: &DiGraph<N, E>) -> ShortestPaths {
+        let n = graph.node_count();
+        let mut dists = vec![vec![f64::INFINITY; n]; n + 1];
+        let mut preds = vec![vec![None; n]; n + 1];
+
+        // TODO add assertion of ShortestPathsByEdge
+        assert_eq!(self.dists.len(), n);
+        assert_eq!(self.preds.len(), n);
+        for k in 0..n {
+            // TODO
+        }
+        ShortestPaths { dists, preds }
+    }
+}
+
+///
+/// # Inputs
+///
+/// Directed graph with edge attribute E has f64 weight.
+///
+pub fn shortest_paths_by_edge<N, E, F>(
+    graph: &DiGraph<N, E>,
+    source: NodeIndex,
+    edge_moveable: F,
+) -> ShortestPathsByEdge
+where
+    E: FloatWeight,
+    F: Fn(EdgeIndex, EdgeIndex) -> bool,
+{
+    let n_nodes = graph.node_count();
+    let n_edges = graph.edge_count();
+    let ix = |edge: EdgeIndex| edge.index();
+    let eps = f64::EPSILON;
+
+    // (1) Initialize
+    //     e
+    // s ----> *
+    let mut dists = vec![vec![f64::INFINITY; n_edges]; n_nodes];
+    let mut preds = vec![vec![None; n_edges]; n_nodes];
+    for edge in graph.edges_directed(source, Direction::Outgoing) {
+        dists[0][ix(edge.id())] = edge.weight().float_weight();
+    }
+
+    // (2) Update
+    for k in 1..n_nodes {
+        // for each edge
+        // * weight w
+        // * from a to b
+        for edge in graph.edge_references() {
+            let v = edge.source();
+            let w = edge.weight().float_weight();
+            let e = edge.id();
+
+            for edge_pred in graph.edges_directed(v, Direction::Incoming) {
+                let ep = edge_pred.id();
+
+                if edge_moveable(ep, e) {
+                    //  ep       e
+                    // ----> v ----> *
+                    if dists[k - 1][ix(ep)] + w + eps < dists[k][ix(e)] {
+                        dists[k][ix(e)] = dists[k - 1][ix(ep)] + w;
+                        preds[k][ix(e)] = Some((ep));
+                    }
+                }
+            }
+        }
+    }
+
+    ShortestPathsByEdge { dists, preds }
 }
 
 ///
