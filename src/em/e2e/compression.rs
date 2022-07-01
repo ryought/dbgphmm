85cb2b6d46ac2ff982252f3a3243e29480d9a045
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-07-01 12:27:08 +0900

    add tandem repeat test

--- src/em/e2e/compression.rs
+++ src/em/e2e/compression.rs
@@ -20,7 +20,7 @@ pub fn inspect_compression_logs<N: DbgNode, E: DbgEdge>(
     for (iteration, log) in logs.iter().enumerate() {
         // println!("log={}", log);
         println!(
-            "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
+            "{}\t{}\t{}\t{}\t{}\t{}\t{}",
             iteration,
             log.p.to_log_value(),
             log.q0,
@@ -28,7 +28,7 @@ pub fn inspect_compression_logs<N: DbgNode, E: DbgEdge>(
             log.cost_diff,
             log.dbg.genome_size(),
             log.dbg.kmer_hists_from_seqs(genome),
-            log.dbg,
+            // log.dbg,
         );
     }
 }
@@ -39,10 +39,11 @@ mod tests {
     use crate::common::Seq;
     use crate::genome;
     use crate::hmmv2::params::PHMMParams;
+
     #[test]
     fn e2e_compression_simple() {
         // data generation
-        let (genome, genome_size) = genome::simple(1000, 5);
+        let (genome, genome_size) = genome::simple(200, 5);
         let dataset = generate_dataset(
             &genome,
             genome_size,
@@ -65,9 +66,44 @@ mod tests {
             &dataset.reads,
             &dataset.phmm_params,
             genome_size,
-            1.0,
+            0.1,
             10,
             10,
+        );
+        inspect_compression_logs(&logs, &genome);
+        println!("dbg_opt={}", new_dbg);
+        println!("dbg_tur={}", dataset.dbg_true_init);
+    }
+
+    #[test]
+    fn e2e_compression_tandem_repeat_diploid() {
+        // data generation
+        let (genome, genome_size) = genome::tandem_repeat_diploid(20, 20, 0.1, 0, 0, 0.01, 0);
+        let dataset = generate_dataset(
+            &genome,
+            genome_size,
+            0,
+            PHMMParams::default(),
+            20, // coverage
+            2000,
+            ReadType::FullLength,
+            8,
+            32,
+        );
+        println!("genome: {}", genome[0].to_str());
+        dataset.show_reads();
+        println!("dbg_raw:{}", dataset.dbg_raw);
+        println!("dbg_true_init:{}", dataset.dbg_true_init);
+
+        // optimize
+        let (new_dbg, logs) = compression(
+            &dataset.dbg_raw,
+            &dataset.reads,
+            &dataset.phmm_params,
+            genome_size,
+            0.1,
+            50,
+            50,
         );
         inspect_compression_logs(&logs, &genome);
         println!("dbg_opt={}", new_dbg);
