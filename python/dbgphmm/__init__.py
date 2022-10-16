517ec29cb074b01937c09c8a6eb4eb49d4572e9f
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-10-16 21:54:34 +0900

    add test() in python __init__

--- python/dbgphmm/__init__.py
+++ python/dbgphmm/__init__.py
@@ -1,4 +1,24 @@
-from .dbgphmm import string_concat
+from .dbgphmm import *
+import networkx as nx
 
 def hoge():
     print('hoge')
+
+def to_networkx(dbg: PyDbg):
+    G = nx.DiGraph()
+    nodes = dbg.nodes()
+    edges = dbg.edges()
+    for (kmer, copy_num) in nodes:
+        G.add_node(kmer, copy_num=copy_num)
+    for (source, target, copy_num) in edges:
+        G.add_edge(nodes[source][0], nodes[target][0], copy_num=copy_num)
+    return G
+
+def test():
+    dbg = PyDbg(4, [
+        StyledSequence("L:ATCGATTCGATTTAG"),
+    ])
+    print(dbg.k, dbg.n_nodes, dbg.n_edges)
+    print(dbg.nodes())
+    G = to_networkx(dbg)
+    print(G.edges)
