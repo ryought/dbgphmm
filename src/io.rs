037e8c4b66c28d1703789fc7ecd4c6604e35078d
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-10-19 15:57:26 +0900

    added `write_string`

--- src/io.rs
+++ src/io.rs
@@ -1,3 +1,15 @@
 pub mod cytoscape;
 pub mod fasta;
 pub mod json;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+///
+/// write string into a file
+///
+pub fn write_string(filename: &str, string: &str) -> std::io::Result<()> {
+    let mut file = File::create(&filename)?;
+    file.write_all(string.as_bytes())?;
+    Ok(())
+}
