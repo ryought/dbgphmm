6fbf7ad48386a75040213021e0b587fd53d23666
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-11-24 15:32:15 +0900

    update submit.sh

--- scripts/job/submit.sh
+++ scripts/job/submit.sh
@@ -9,7 +9,7 @@ case $1 in
   "case3")
     ARG="-c 20 -l 100 -p 0.01 --k-init 12 --k-final 100 -U 50 -N 20 -E 50 -P 2 -D 0.05 -H 0.05 --sigma 100 -m 3 --use-fragment-read";;
   "case4")
-    ARG="-c 15 -l 100 -p 0.001 --k-init 24 --k-final 100 -U 50 -N 10 -E 50 -P 2 -D 0.05 -H 0.05 --sigma 100 -m 3 --use-fragment-read";;
+    ARG="-c 15 -l 100 -p 0.001 --k-init 16 --k-final 100 -U 50 -N 10 -E 50 -P 2 -D 0.05 -H 0.05 --sigma 100 -m 3 --use-fragment-read";;
   "case5")
     ARG="-c 10 -l 200 -p 0.001 --k-init 40 --k-final 200 -U 2000 -N 1 -E 0 -P 2 -D 0.05 -H 0.05 --sigma 100 -m 3 --use-fragment-read";;
   *)
