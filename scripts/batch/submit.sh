4b79a1e0cfb9d484885ff95aca8bbcfd91f0b169
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-01-19 08:51:07 +0900

    update batch/submit.sh (add param p_error)

--- scripts/batch/submit.sh
+++ scripts/batch/submit.sh
@@ -6,11 +6,14 @@ for U in 2000 1000 500 100 50 20
 do
   L=2000
   N=$(($L / $U))
-  for H in 0.001 0.005 0.01
+  for H in 0.001 0.005
   do
-    ARG="-U $U -N $N -H $H -P 2"
-    OUTPUT="U${U}N${N}H${H//./}P2"
-    echo $ARG $OUTPUT
-    pjsub -x "ARG=$ARG,OUTPUT=$OUTPUT" -N $OUTPUT -o $OUTPUT.log -j job.sh
+    for p in 0.001 0.005
+    do
+      ARG="-U $U -N $N -H $H -P 2 -p $p"
+      OUTPUT="U${U}N${N}H${H//./}P2p${p//./}"
+      echo $ARG $OUTPUT
+      pjsub -x "ARG=$ARG,OUTPUT=$OUTPUT" -N $OUTPUT -o $OUTPUT.log -j job.sh
+    done
   done
 done
