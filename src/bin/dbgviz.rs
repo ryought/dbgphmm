d467d95e5aaf5fc1d891341cf8ae47029984718d
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-01-18 15:47:18 +0900

    add dbgviz bin

--- src/bin/dbgviz.rs
+++ src/bin/dbgviz.rs
@@ -1,10 +1,56 @@
+use clap::Clap;
 use dbgphmm::common::{ni, Reads};
 use dbgphmm::dbg::float::{q_score_diff_exact, CopyDensity, FloatDbg, FloatDbgEdge, FloatDbgNode};
 use dbgphmm::dbg::mocks::mock_intersection_small;
+use dbgphmm::dbg::{Dbg, SimpleDbg};
+use dbgphmm::genome;
+use dbgphmm::kmer::VecKmer;
 use dbgphmm::prelude::*;
 use dbgphmm::vector::{DenseStorage, NodeVec};
 
+///
+/// Generate genome de Bruijn graph for dbgviz visualization
+///
+#[derive(Clap, Debug)]
+struct Opts {
+    ///
+    #[clap(short = 'k')]
+    k: usize,
+    ///
+    #[clap(short = 'U')]
+    unit_size: usize,
+    ///
+    #[clap(short = 'N')]
+    n_unit: usize,
+    ///
+    #[clap(short = 'H', default_value = "0.01")]
+    hap_divergence: f64,
+    ///
+    #[clap(short = 'P', default_value = "1")]
+    n_haplotypes: usize,
+    ///
+    #[clap(short = 's')]
+    seed: u64,
+}
+
 fn main() {
+    let opts: Opts = Opts::parse();
+    // data generation
+    let (genome, genome_size) = genome::tandem_repeat_polyploid_with_unique_homo_ends(
+        opts.unit_size,
+        opts.n_unit,
+        opts.seed,
+        50,
+        opts.n_haplotypes,
+        opts.hap_divergence,
+        opts.seed,
+    );
+    let dbg: SimpleDbg<VecKmer> = SimpleDbg::from_styled_seqs(opts.k, &genome);
+    let json = dbg.to_cytoscape();
+    println!("{}", json);
+}
+
+fn main_old() {
     let dbg = mock_intersection_small();
     let genome_size = dbg.genome_size() as CopyDensity;
     let reads = Reads::from(vec![b"ATAGCT".to_vec()]);
