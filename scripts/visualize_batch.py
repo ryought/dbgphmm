22c054d3321bb9294ebdb4fb20e032a8a3072548
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-01-19 15:21:39 +0900

    generate fig_1,2,supp2

--- scripts/visualize_batch.py
+++ scripts/visualize_batch.py
@@ -9,10 +9,19 @@ from parse import *
 from pathlib import Path
 import math
 import numpy as np
+from itertools import product
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
 from visualize_posterior import parse_prob
 import pickle
+
+Us = [1000, 500, 100, 50, 20]
+L = 1000
+P = 2
+Hs = [0.001, 0.005]
+H = 0.001
+ps = [0.001, 0.005, 0.01]
+markers = ['+', '*', 'x']
 
 @dataclass
 class Experiment:
@@ -85,24 +94,125 @@ def defaultdict_to_dict(d):
             d[k] = defaultdict_to_dict(v)
     return dict(d)
 
+def fig_1(kmers, samples, filename=None):
+    """
+    kmer mis-classification as zero-copy rate
+
+    x-axis: k
+    y-axis: P(c[v]=0) s.t. c_true[v]=1
+    """
+    plt.figure(figsize=(10, 10))
+    for i, (U, p) in enumerate(product(Us, ps)):
+        plt.subplot(len(Us), len(ps), i + 1)
+        for (seed, d) in kmers[(U, H, p)].items():
+            xs = []
+            ys = []
+            for (k, dd) in d.items():
+                for kmer in dd:
+                    if kmer.copy_num_true == 1:
+                        xs.append(k)
+                        ys.append(kmer.p_zero)
+            plt.scatter(xs, ys, marker=markers[seed], alpha=0.5)
+        plt.ylim(0, 1)
+        plt.xlim(10, 50)
+        plt.xlabel('k')
+        plt.ylabel('P(c[v]=0) c*[v]=1')
+        plt.title('U={} p={}'.format(U, p))
+    if filename:
+        plt.savefig(filename, dpi=200)
+    else:
+        plt.show()
+
+
+def fig_2(kmers, samples, filename=None):
+    """
+    missing k-mer count and model likelihood
+
+    x-axis: k
+    y-axis: P(c[v]=0) s.t. c_true[v]=1
+    """
+    # Us = [500]
+    # ps = [0.01]
+    plt.figure(figsize=(10, 10))
+    plt.suptitle('Log likelihood difference between best models w/ and w/o missing kmers')
+    for i, (U, p) in enumerate(product(Us, ps)):
+        plt.subplot(len(Us), len(ps), i + 1)
+        plt.title('U={} p={}'.format(U, p))
+        plt.ylim(-10, 10)
+        plt.grid()
+        for seed in samples[(U, H, p)].keys():
+            xs = []
+            ys = []
+            for (k, dd) in samples[(U, H, p)][seed].items():
+                x0 = max(sample.log_likelihood for sample in dd if sample.n_missing == 0)
+                x1 = max(sample.log_likelihood for sample in dd if sample.n_missing > 0)
+                xs.append(k)
+                ys.append(x0 - x1)
+            plt.scatter(xs, ys)
+    if filename:
+        plt.savefig(filename, dpi=200)
+    else:
+        plt.show()
+    plt.clf()
+    plt.close()
+
+
+def fig_supp2(kmers, samples, filename=None):
+    """
+    missing k-mer count and model likelihood
+    show the distribution of missing-kmer and likelihood of sampled models
+    for each seed and k value.
+
+    x-axis: n_missings
+    y-axis: log_likelihood
+    """
+    # Us = [500]
+    # ps = [0.01]
+    for _, (U, p) in enumerate(product(Us, ps)):
+        plt.figure(figsize=(20, 10))
+        plt.suptitle('n_missing vs log_likelihood (U={} p={})'.format(U, p))
+        seeds = list(samples[(U, H, p)].keys())
+        ks = list(samples[(U, H, p)][seeds[0]].keys())
+        for i, (seed, k) in enumerate(product(seeds, ks)):
+                plt.subplot(len(seeds), len(ks), i + 1)
+                xs = []
+                ys = []
+                for sample in samples[(U, H, p)][seed][k]:
+                    xs.append(sample.n_missing)
+                    ys.append(sample.log_likelihood)
+                plt.scatter(xs, ys, alpha=0.5)
+                plt.xlim(0, 100)
+                plt.title('seed={} k={}'.format(seed, k))
+
+        if filename:
+            plt.savefig(filename.format(U=U, p=p))
+        else:
+            plt.show()
+        plt.clf()
+        plt.close()
+
 def main():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('log_dir', type=Path, help='batch log directory')
+    parser.add_argument('--to-pickle', type=Path, help='batch log directory')
     args = parser.parse_args()
 
     kmers = dict()
     samples = dict()
-    for U in [1000, 500, 100, 50, 20]:
-        N = 1000 // U
-        P = 2
-        # for H in [0.001, 0.005]:
-        H = 0.001
-        for p in [0.001, 0.005, 0.01]:
-            kmers, samples, opts = parse_log_file(args.log_dir / filename_from_params(U, N, H, P, p))
-            kmers[(U, H, p)] = defaultdict_to_dict(kmers)
-            samples[(U, H, p)] = defaultdict_to_dict(samples)
-    with open('batch_logs.pkl', 'wb') as f:
-        pickle.dump((kmers, samples), f)
+    for U in Us:
+        N = L // U
+        for p in ps:
+            k, s, o = parse_log_file(args.log_dir / filename_from_params(U, N, H, P, p))
+            kmers[(U, H, p)] = defaultdict_to_dict(k)
+            samples[(U, H, p)] = defaultdict_to_dict(s)
+
+    if args.to_pickle:
+        with open(args.to_pickle, 'wb') as f:
+            pickle.dump((kmers, samples), f)
+
+    fig_1(kmers, samples, filename='fig_1.png')
+    fig_2(kmers, samples, filename='fig_2.png')
+    fig_supp2(kmers, samples, filename='fig_supp2_U{U}p{p}.png')
 
 if __name__ == '__main__':
     main()
