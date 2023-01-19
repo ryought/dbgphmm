11125709e3208202e303b7263c980d608931b8db
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-01-19 11:46:14 +0900

    new submit

--- scripts/batch/submit.sh
+++ scripts/batch/submit.sh
@@ -2,15 +2,15 @@
 set -Ceuo pipefail
 cargo build --release
 # cargo run --release --bin batch -- -H 0.01 -U 100 -N 10 -P 2
-for U in 2000 1000 500 100 50 20
+for U in 1000 500 100 50 20
 do
-  L=2000
+  L=1000
   N=$(($L / $U))
   for H in 0.001 0.005
   do
-    for p in 0.001 0.005
+    for p in 0.001 0.005 0.01
     do
-      ARG="-U $U -N $N -H $H -P 2 -p $p"
+      ARG="-U $U -N $N -H $H -P 2 -p $p --try-all-k"
       OUTPUT="U${U}N${N}H${H//./}P2p${p//./}"
       echo $ARG $OUTPUT
       pjsub -x "ARG=$ARG,OUTPUT=$OUTPUT" -N $OUTPUT -o $OUTPUT.log -j job.sh
