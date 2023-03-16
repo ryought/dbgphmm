0ac08016cf2d248106b59f89bfaedcb1303c5c33
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-03-16 13:35:57 +0900

    add more test

--- src/hmmv2/speed.rs
+++ src/hmmv2/speed.rs
@@ -107,19 +107,28 @@ mod tests {
         // test using full-length error-free read
         //
         let p_true = lp(-138.0);
-        // dense
+        // dense ~0.6sec
         let (p, time) = timer(|| phmm.to_full_prob(&genome));
         println!("p={} t={}", p, time);
         assert!(p.log_diff(p_true) < 1.0);
-        // sparse
+        // sparse ~0.3sec
         let (p, time) = timer(|| phmm.to_full_prob_sparse(&genome));
         println!("p={} t={}", p, time);
         assert!(p.log_diff(p_true) < 1.0);
 
         //
-        // test using full-length error-free read
+        // test using fragment and errorneous read
         //
-        let (p, time) = timer(|| phmm.to_full_prob_sparse(dataset.reads()));
+        println!("read");
+        // dense
+        let (p, time) = timer(|| phmm.to_full_prob(dataset.reads()));
+        let p_true = lp(-1448.0);
         println!("p={} t={}", p, time);
+        assert!(p.log_diff(p_true) < 1.0);
+        // sparse
+        let (p, time) = timer(|| phmm.to_full_prob_sparse(dataset.reads()));
+        let p_true = lp(-1475.6);
+        println!("p={} t={}", p, time);
+        assert!(p.log_diff(p_true) < 1.0);
     }
 }
