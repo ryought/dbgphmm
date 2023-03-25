209601e063d27e024db3b25930f192603f9d771e
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-03-25 18:16:52 +0900

    large 10k diploid test

--- scripts/r.sh
+++ scripts/r.sh
@@ -15,5 +15,9 @@ module load python/3.7.3
 # ./target/release/infer -k 16 -K 500 -p 0.001 --dataset-json r/u1k_n1.json --output-prefix r/u1k_n1 -I 50
 
 # ./target/release/draft -k 20 -C 20 -L 400 -p 0.005 -U 200 -N 10 -E 200 -H 0.03 -P 2 --output-prefix r/u200_n10
-./target/release/infer -k 20 -K 40 -p 0.001 --dataset-json r/u200_n10.json --output-prefix r/u200_n10_v3 -I 50
+# ./target/release/infer -k 20 -K 40 -p 0.001 --dataset-json r/u200_n10.json --output-prefix r/u200_n10_v3 -I 50
 # ./target/release/infer -k 20 -K 40 -p 0.0001 --dataset-json r/u200_n10.json --output-prefix r/u200_n10_p00001 -I 50
+
+KEY="u10k_n2"
+./target/release/draft -k 20 -C 20 -L 20000 -p 0.005 -U 10000 -N 2 -E 1000 -H 0.03 -P 2 --output-prefix r/$KEY
+./target/release/infer -K 20000 -p 0.001 -I 50 --dataset-json r/$KEY.json --dbg r/$KEY.dbg --output-prefix r/$KEY
