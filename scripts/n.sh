307b59dc15a5c1977d4951cd7edada9a86396131
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-03-26 15:05:29 +0900

    update n.sh

--- scripts/n.sh
+++ scripts/n.sh
@@ -4,15 +4,21 @@
 #PJM -L elapse=24:00:00
 #PJM -L rscgrp=gg57
 
-#
-# To submit:
-#
-# cargo build --release
-# pjsub -X N=1 -N N1 -j n.sh
-#
+if [ $1 = "sub" ]
+then
+  # submitter
+  echo "pjsub"
 
-cd /work/00/gg57/j29006/dbgphmm
-module load python/3.7.3
-echo "Running N=$N"
-./target/release/draft -k 20 -C 20 -L 1000 -p 0.01 -U 500 -N $N -E 300 -H 0.02 -P 2 --output-prefix n/u500_n$N
-./target/release/infer -K 1000 -p 0.001 -I 50 --dataset-json n/u500_n$N.json --output-prefix n/u500_n$N
+  cargo build --release
+  mkdir -p n
+  pjsub -x N=1 -N N1 -j scripts/n.sh
+else
+  # runner
+  #
+  cd /work/00/gg57/j29006/dbgphmm
+  module load python/3.7.3
+  echo "Running N=$N"
+  ./target/release/draft -k 20 -C 20 -L 1000 -p 0.01 -U 500 -N $N -E 300 -H 0.02 -P 2 --output-prefix n/u500_n$N
+  ./target/release/infer -k 20 -K 1000 -p 0.001 -I 50 --dataset-json n/u500_n$N.json --output-prefix n/u500_n$N
+fi
+
