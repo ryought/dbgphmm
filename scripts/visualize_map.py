e38422df195d8481e71a25a13cba2d9c79bf6d99
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-05-10 14:23:57 +0900

    add `scripts/visualize_search_history.py`

--- scripts/visualize_map.py
+++ scripts/visualize_map.py
@@ -3,6 +3,7 @@
 """
 Visualize difference of MAP file (dbgphmm)
 """
+from dbgphmm import parse_dbg_file, parse_map_file, jaccards
 import csv
 import matplotlib.pyplot as plt
 from typing import List, Tuple, Dict
@@ -12,98 +13,7 @@ from dataclasses import dataclass
 import re
 import numpy as np
 import glob
-import networkx as nx
 csv.field_size_limit(1000000000)
-
-
-@dataclass(frozen=True)
-class Emit:
-    node: int
-    prob: float
-
-
-def parse_emits_list(s: str) -> List[Emit]:
-    """
-    >>> a = parse_emits_list('42:99.1,3:0.8,1:0.0,24:0.0')
-    >>> a == [Emit(node=42, prob=99.1), Emit(node=3, prob=0.8), Emit(node=1, prob=0.0), Emit(node=24, prob=0.0)]
-    True
-    """
-    return [Emit(node=int(t.split(':')[0]), prob=float(t.split(':')[1]))
-            for t in s.split(',')]
-
-
-def parse_map_file(filename: Path) -> List[List[List[Emit]]]:
-    """
-    """
-    ret = []
-    current_read = None
-    currens_pos = None
-    with open(filename) as f:
-        for line in f:
-            if line[0] == '#':
-                continue
-            s = line.rstrip().split('\t')
-            if s:
-                read = int(s[0])
-                pos = int(s[1])
-                base = s[2]
-                emits = parse_emits_list(s[3])
-
-                if current_read != read:
-                    print(current_read)
-                    ret.append([])
-                ret[read].append(emits)
-                current_read = read
-                current_pos = pos
-    return ret
-
-
-def jaccard(a: List[Emit], b: List[Emit]) -> float:
-    """
-    >>> a = [Emit(node=42, prob=99.1), Emit(node=3, prob=0.8), Emit(node=1, prob=0.0)]
-    >>> b = [Emit(node=42, prob=99.1), Emit(node=3, prob=0.8), Emit(node=0, prob=0.0)]
-    >>> jaccard(a, b) == 2/4
-    True
-    """
-    sa = set(e.node for e in a)
-    sb = set(e.node for e in b)
-    return len(sa & sb) / len(sa | sb)
-
-
-def jaccards(map_a, map_b):
-    ret = []
-    n_reads = len(map_a)
-    for i in range(n_reads):
-        s = [jaccard(map_a[i][j], map_b[i][j]) for j in range(len(map_a[i]))]
-        ret.append(s)
-    return ret
-
-
-def parse_dbg_file(filename: Path):
-    """
-    """
-    g = nx.MultiDiGraph()
-    with open(filename) as f:
-        for line in f:
-            if line[0] == '#':
-                continue
-            s = line.rstrip().split('\t')
-            if s[0] == 'K':
-                k = int(s[1])
-            elif s[0] == 'N':
-                node = int(s[1])
-                kmer = s[2]
-                g.add_node(node, id=node, kmer=kmer)
-            elif s[0] == 'E':
-                edge = int(s[1])
-                source = int(s[2])
-                target = int(s[3])
-                kmer = s[4]
-                copy_num = int(s[5])
-                full_edges = [int(t) for t in s[6].split(',')]
-                g.add_edge(source, target, id=edge, kmer=kmer,
-                           copy_num=copy_num, full_edges=full_edges)
-    return g
 
 
 def edge_map(dbg):
