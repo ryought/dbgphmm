12f49b94905ed98508a07d8b9bcca2d0ab06bc2a
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-08-05 17:41:23 +0900

    update job/n

--- scripts/n.sh
+++ scripts/n.sh
@@ -14,29 +14,32 @@ then
 
   cargo build --release --features intel --no-default-features
   mkdir -p n3
-  # for N in 10
-  # for N in 1 2 3 4 5 6
-  # for N in 12
-  # for N in 50 200
-  # for N in 10 11 12
-  # for N in 8 9 10 11 12
-  # for N in 100
-  # u100
-  # for N in 10 20 50 100
-  # u500
-  # for N in 1 2 3 4 5 6 7 8 9 10 11 12
-  # u20
-  for N in 50 100 200
-  do
-    echo $N
-    # Name:
-    # - key
-    # - git-hash
-    # Parameter:
-    # - N
-    # Compile before run
-    pjsub -x N=$N -N n3_p01_u20_n$N -j scripts/n.sh
-  done
+
+  if [ $2 = "u100" ]
+  then
+    # u100
+    for N in 10 20 50 100
+    do
+      echo $N
+      pjsub -x N=$N,U=100 -N n3_p01_u100_n$N -j scripts/n.sh
+    done
+  elif [ $2 = "u500" ]
+  then
+    # u500
+    for N in 1 2 3 4 5 6 7 8 9 10 11 12
+    do
+      echo $N
+      pjsub -x N=$N,U=500 -N n3_p01_u500_n$N -j scripts/n.sh
+    done
+  elif [ $2 = "u20" ]
+  then
+    # u20
+    for N in 50 100 200
+    do
+      echo $N
+      pjsub -x N=$N,U=20 -N n3_p01_u20_n$N -j scripts/n.sh
+    done
+  fi
 else
   #
   # runner
@@ -44,28 +47,25 @@ else
   cd /work/00/gg57/j29006/dbgphmm
   module load python/3.7.3
   export OMP_NUM_THREADS=1
-  echo "Running N=$N"
-  # ./target/release/draft -k 20 -C 20 -L 1000 -p 0.001 -U 500 -N $N -E 300 -H 0.02 -P 2 --output-prefix n/p01_u500_n$N
-  # ./target/release/infer -k 20 -K 1000 -p 0.0001 -I 50 --dataset-json n/p01_u500_n$N.json --output-prefix n/p01_u500_n$N
-  # ./target/release/infer -k 20 -K 1000 -p 0.00001 -I 50 --dataset-json n/p01_u500_n$N.json --output-prefix n/p01_u500_n${N}_pi
-  # ./target/release/infer -k 20 -K 1000 -p 0.00001 -I 50 --dataset-json n/p01_u500_n$N.json --output-prefix n/p01_u500_n${N}_pi_v2
-  # ./target/release/infer -k 40 -K 1000 -p 0.00001 -I 50 --dataset-json n/p01_u500_n$N.json --output-prefix n/p01_u500_n${N}_pi_v3
-
-  # store correspondence output-data and day-or-parameters
-  #
+  echo "Running N=$N U=$U"
 
   # p01
-  # u500
-  # ./target/release/draft -k 40 -C 20 -L 1000 -p 0.001 -U 500 -N $N -E 300 -H 0.02 --H0 0.02 -P 2 --output-prefix n3/p01_u500_n$N
-  # ./target/release/infer -k 40 -K 1000 -p 0.0001 -e 0.001 -I 50 -s 500 --km 1000 --kr 1000 --dataset-json n3/p01_u500_n$N.json --output-prefix n3/p01_u500_n$N
-
-  # u20
-  ./target/release/draft -k 40 -C 20 -L 1000 -p 0.001 -U 20 -N $N -E 300 -H 0.02 --H0 0.02 -P 2 --output-prefix n3/p01_u20_n$N
-  ./target/release/infer -k 40 -K 1000 -p 0.00001 -e 0.001 -I 50 -s 500 --km 1000 --kr 1000 --dataset-json n3/p01_u20_n$N.json --output-prefix n3/p01_u20_n$N
-
-  # u100
-  # ./target/release/draft -k 40 -C 20 -L 1000 -p 0.001 -U 100 -N $N -E 300 -H 0.02 --H0 0.02 -P 2 --output-prefix n3/p01_u100_n$N
-  # ./target/release/infer -k 40 -K 1000 -p 0.00001 -e 0.001 -I 50 -s 500 --km 1000 --kr 1000 --dataset-json n3/p01_u100_n$N.json --output-prefix n3/p01_u100_n$N
+  if [ $U = 500 ]
+  then
+    # u500
+    ./target/release/draft -k 40 -C 20 -L 1000 -p 0.001 -U 500 -N $N -E 300 -H 0.02 --H0 0.02 -P 2 --output-prefix n3/p01_u500_n$N
+    ./target/release/infer -k 40 -K 1000 -p 0.0001 -e 0.001 -I 50 -s 500 --km 1000 --kr 1000 --dataset-json n3/p01_u500_n$N.json --output-prefix n3/p01_u500_n$N
+  elif [ $U = 20 ]
+  then
+    # u20
+    ./target/release/draft -k 40 -C 20 -L 1000 -p 0.001 -U 20 -N $N -E 300 -H 0.02 --H0 0.02 -P 2 --output-prefix n3/p01_u20_n$N
+    ./target/release/infer -k 40 -K 1000 -p 0.00001 -e 0.001 -I 50 -s 500 --km 1000 --kr 1000 --dataset-json n3/p01_u20_n$N.json --output-prefix n3/p01_u20_n$N
+  elif [ $U = 100 ]
+  then
+    # u100
+    ./target/release/draft -k 40 -C 20 -L 1000 -p 0.001 -U 100 -N $N -E 300 -H 0.02 --H0 0.02 -P 2 --output-prefix n3/p01_u100_n$N
+    ./target/release/infer -k 40 -K 1000 -p 0.00001 -e 0.001 -I 50 -s 500 --km 1000 --kr 1000 --dataset-json n3/p01_u100_n$N.json --output-prefix n3/p01_u100_n$N
+  fi
 
   # p1
   # ./target/release/draft -k 16 -C 20 -L 1000 -p 0.01 -U 500 -N $N -E 300 -H 0.02 --H0 0.02 -P 2 --output-prefix n3/p1_u500_n$N
