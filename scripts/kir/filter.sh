39836a490a4c83a48e08d1376ebb533c7376fa6c
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-09-06 14:17:42 +0900

    updated kir filter script

--- scripts/kir/filter.sh
+++ scripts/kir/filter.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 set -Ceuo pipefail
 
-REF='KIR_Primary.fa'
+# Test 1: use
+# REF='KIR_Primary.fa'
+# at least 2000 bp
+# CUTOFF=2000
+# TR region
+# L=680000
+# R=860000
+
+
+# Test 2
+REF='chr19.fa'
 CUTOFF=2000
-# TR region [690k:860k]
-L=690000
-R=860000
+# LRC_KIR region
+START=54025634
+END=55084318
+# tandem repeat region in chr19
+# 680 kb : 860 kb
+L=$(( $START + 680000 ))
+R=$(( $START + 860000 ))
+# 54,705 kb : 54,905 kb
+# 54,896 : 54,900 kb
+# 868 874
+
+
+cd ~/work/dbgphmm/kir
+
+function map () {
+  READS=$1
+  BASENAME=$(basename $READS)
+  KEY=${BASENAME%.fastq.gz}
+  PAF="$KEY.paf"
+
+  echo $KEY $PAF
+  # [ ! -e $PAF ] && minimap2 -t 16 -x map-pb -o $PAF $REF $READS
+  minimap2 -t 16 -x asm20 -o $PAF $REF $READS
+}
 
 function filter () {
   READS=$1
@@ -13,22 +44,41 @@ function filter () {
   KEY=${BASENAME%.fastq.gz}
   PAF="$KEY.paf"
 
-  echo $KEY $PAF
+  # forward
+  echo forward
+  gawk "\$8 <= $R && \$9 >= $L && \$10>=$CUTOFF && \$5 == \"+\"" $PAF | cut -f1 | sort | uniq > $KEY.f.ids
+  # backward
+  echo backward
+  gawk "\$8 <= $R && \$9 >= $L && \$10>=$CUTOFF && \$5 == \"-\"" $PAF | cut -f1 | sort | uniq > $KEY.b.ids
 
-  # 1
-  [ ! -e $PAF ] && minimap2 -t 16 -x map-pb -o $PAF $REF $READS
-  cat $PAF | gawk "\$6 == \"LRC_KIR_Primary\" && \$8 <= $R && \$9 >= $L && \$10>=$CUTOFF" | cut -f1 | sort | uniq > $KEY.C$CUTOFF.ids
 
   # 2
   # seqkit fq2fa $READS | seqkit seq -m 1000 > ${ROOT}.all.fa
-  seqkit faidx /work/ryought/mhc/data/KIR/filter/$KEY.all.fa --infile-list $KEY.C$CUTOFF.ids > $KEY.KIR.C$CUTOFF.fa
+  # forward
+  seqkit faidx /work/ryought/mhc/data/KIR/filter/$KEY.all.fa --infile-list $KEY.f.ids > $KEY.f.fa
+  seqkit faidx /work/ryought/mhc/data/KIR/filter/$KEY.all.fa --infile-list $KEY.b.ids | seqkit seq -rpv -t dna > $KEY.b.fa
+}
+
+function mapping_with_igv () {
+  READS=$1
+  echo 'mapping'
+  minimap2 -c -x asm20 --cs -a $REF $READS > $READS.sam
+  echo 'converting bam'
+  samtools view -bS $READS.sam | samtools sort > $READS.bam
+  samtools index $READS.bam
+}
+
+function merge () {
+  cat *.f.fa *.b.fa > merged.fa
 }
 
 find /data/HG002/PacBio_CCS_15kb_20kb_chemistry2/*.fastq.gz | while read READS
 do
   echo $READS
-  filter $READS
+  # map $READS
+  # filter $READS
 done
 
 # merge
-cat *.KIR.C$CUTOFF.fa > reads.C$CUTOFF.fa
+
+mapping_with_igv merged.fa
