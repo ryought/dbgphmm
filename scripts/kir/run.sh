fa0f4517f824e8e90fb364b3322ba9909a0ab8d7
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-09-14 12:08:55 +0900

    update kir/haps/10x jobscript

--- scripts/kir/run.sh
+++ scripts/kir/run.sh
@@ -20,9 +20,15 @@ module load python/3.7.3
 export OMP_NUM_THREADS=1
 
 # (1) kir/haps
+# 20x
 # ./target/release/draft -k 40 --genome-fasta kir/haps01.fa -C 20 -L 20000 -p 0.001 -U 10 -N 10 -E 10 -H 0.0 --H0 0.0 -P 2 --output-prefix kir/haps/v0
+# ./target/release/infer -k 40 -K 20000 -p 0.00001 -e 0.001 -I 50 -s 10000 --dataset-json kir/haps/v0.json --output-prefix kir/haps/v0
+# 10x draft was wrong
 # ./target/release/draft -k 40 --genome-fasta kir/haps/haps01.fa -C 10 -L 20000 -p 0.001 -U 10 -N 10 -E 10 -H 0.0 --H0 0.0 -P 2 --output-prefix kir/haps/10x/v0
 # ./target/release/infer -k 40 -K 20000 -p 0.00001 -e 0.001 -I 50 -s 10000 --dataset-json kir/haps/10x/v0.json --output-prefix kir/haps/10x/v0
+# 10x
+./target/release/draft -k 32 -M 3 --genome-fasta kir/haps01.fa -C 10 -L 20000 -p 0.001 -U 10 -N 10 -E 10 -H 0.0 --H0 0.0 -P 2 --output-prefix kir/haps/10x/v0
+./target/release/infer --dbg kir/haps/10x/v0.dbg -K 20000 -p 0.00001 -e 0.001 -I 50 -s 10000 --dataset-json kir/haps/10x/v0.json --output-prefix kir/haps/10x/v0
 
 # (2) kir/hifi
 # ./target/release/dbgphmm draft -k 40 -G 360000 -m 2 -M 10 -p 0.001 -P 2 -d kir/hifi/trimed.dbg -g kir/hifi/trimed.gfa kir/hifi/trimed.fa
