7cbbe7520d102e3ede8c30ad58ff0ddc39c93744
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-10-01 18:37:04 +0900

    udpate join

--- scripts/kir/join.py
+++ scripts/kir/join.py
@@ -3,27 +3,38 @@
 from Bio import SeqIO
 import argparse
 from pathlib import Path
+import sys
 
 
 def main():
     parser = argparse.ArgumentParser(description='Join fasta records')
     parser.add_argument('fasta', type=Path, help='FASTA')
-    parser.add_argument('query', type=str,
-                        help='record ids delimited by comma')
+    parser.add_argument('query', type=str, nargs='+',
+                        help='record ids delimited by comma and space')
     parser.add_argument('--name', type=str,
                         default='out',
-                        help='record ids delimited by comma')
+                        help='')
     args = parser.parse_args()
     fasta = SeqIO.to_dict(SeqIO.parse(args.fasta, "fasta"))
-    seq = ''
-    for id in args.query.split(','):
-        if id not in fasta:
-            print('"{}" is not in fasta'.format(id))
-            return
-        seq += fasta[id].seq
 
-    print('>{}'.format(args.name))
-    print(seq)
+    for i, query in enumerate(args.query):
+        seq = ''
+        for q in query.split(','):
+            direction = q[-1]
+            id = q[:-1]
+            if id not in fasta:
+                print('"{}" is not in fasta'.format(id), file=sys.stderr)
+                sys.exit(1)
+            if direction == '+':
+                seq += fasta[id].seq
+            elif direction == '-':
+                seq += fasta[id].seq.reverse_complement()
+            else:
+                print('invalid direction "{}"'.format(
+                    direction), file=sys.stderr)
+                sys.exit(1)
+        print('>{}{}'.format(args.name, i))
+        print(seq)
 
 
 if __name__ == '__main__':
