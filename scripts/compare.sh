e2e47407647877684b4159e671d7753b73e8624e
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2023-10-01 10:49:47 +0900

    update compare

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export OMP_NUM_THREADS=1
 
-
+G=50000
 
 function gfa2fa () {
   GFA=$1
@@ -11,7 +11,7 @@ function gfa2fa () {
 
 function summary_paf () {
   PAF=$1
-  awk 'BEGIN{OFS="\t"} { print $1,$2,$3,$4,$5,$6,$7,$8,$9,$13 }' $1
+  awk 'BEGIN{OFS="\t"} { print $1,$2,$3,$4,$5,$6,$7,$8,$9,$13,$24 }' $1
 }
 
 function run_hifiasm () {
@@ -35,28 +35,39 @@ function run_hifiasm () {
 
 
 
-for C in 3 5 10
-do
-  for U in 10000 5000 2500 1000 500
+function run_v0 () {
+  for C in 3 5 10
   do
-    N=$(( 50000 / $U ))
-    for H in 0.02 0.01 0.001
+    for U in 10000 5000 2500 1000 500
     do
-      KEY="U${U}_N${N}_H${H}_C${C}"
-      echo "$C $U $N $H $KEY"
-      # mkdir -p t/$KEY
+      N=$(( $G / $U ))
+      for H in 0.02 0.01 0.001
+      do
+        KEY="U${U}_N${N}_H${H}_C${C}"
+        echo "$C $U $N $H $KEY"
+        # mkdir -p t/$KEY
 
-      # create dataset
-      # ./target/release/draft -k 40 -C $C -L 10000 -p 0.001 -m 1 -M 2 -U $U -N $N -E 10000 -H $H --H0 $H -P 2 --output-prefix t/$KEY/data --dataset-only
+        # create dataset
+        ./target/release/draft -k 40 -C $C -L 10000 -p 0.001 -m 1 -M 2 -U $U -N $N -E 10000 -H $H --H0 $H -P 2 --output-prefix t/$KEY/data --dataset-only
 
-      # run hifiasm
-      # run_hifiasm $KEY
+        # run hifiasm
+        # run_hifiasm $KEY
 
-      # summary_paf "t/$KEY/hifiasm/out.paf"
-      summary_paf "t/$KEY/hifiasm_opt/out.paf"
+        summary_paf "t/$KEY/hifiasm/out.p_utg.paf"
+        # summary_paf "t/$KEY/hifiasm_opt/out.paf"
+      done
     done
   done
-done
+  # ./target/release/infer -k 40 -p 0.00001 -K 10000 -e 0.001 -I 50 -s 10000 --dbg t/t8.dbg --dataset-json t/t8.json --output-prefix t/t8
+}
 
 
-# ./target/release/infer -k 40 -p 0.00001 -K 10000 -e 0.001 -I 50 -s 10000 --dbg t/t8.dbg --dataset-json t/t8.json --output-prefix t/t8
+function run_dbgphmm () {
+  C=10
+  U=5000
+  H=0.001
+  N=$(( $G / $U ))
+  KEY="U${U}_N${N}_H${H}_C${C}"
+  ./target/release/draft -k 40 -C $C -L 10000 -p 0.001 -M 4 -U $U -N $N -E 10000 -H $H --H0 $H -P 2 --output-prefix t/$KEY/data
+  ./target/release/infer --dbg t/$KEY/data.dbg -K 10000 -p 0.00001 -e 0.001 -s 10000 -I 50 --dataset-json t/$KEY/data.json --output-prefix t/$KEY/v0
+}
